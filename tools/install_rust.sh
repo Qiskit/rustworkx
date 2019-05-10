99a3877d4c7b13635c9de8b54fae06f718f8a6ad
Matthew Treinish <mtreinish@kortar.org>
2019-05-10 18:31:37 -0400

    Update rustup invocation

--- tools/install_rust.sh
+++ tools/install_rust.sh
@@ -1,5 +1,5 @@
 if [ ! -d rust-installer ]; then
     mkdir rust-installer
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rust-installer/rustup.sh
-    sh rust-installer/rustup.sh --prefix=~/rust --spec=nightly --disable-sudo -y
+    sh rust-installer/rustup.sh --default-toolchain nightly -y
 fi
