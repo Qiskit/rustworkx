a78b2d96c795d8165f64caf97738fdec130954f6
Matthew Treinish <mtreinish@kortar.org>
2019-05-11 12:07:53 -0400

    Add tests for edge functions

---
+++ tests/test_edges.py
@@ -0,0 +1,32 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+import retworkx
+
+
+class TestEdges(unittest.TestCase):
+
+    def test_get_edge_data(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        node_b = dag.add_child(node_a, 'b', "Edgy")
+        res = dag.get_edge_data(node_a, node_b)
+        self.assertEqual("Edgy", res)
+
+    def test_no_edge(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        node_b = dag.add_node('b')
+        self.assertRaises(Exception, dag.get_edge_data,
+                          node_a, node_b)
