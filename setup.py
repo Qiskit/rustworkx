302d3b62b348b3658a55f07503a9970ee3e51a7a
Matthew Treinish <mtreinish@kortar.org>
2019-05-31 16:22:04 -0400

    Bump to 0.0.9 for release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="retworkx",
-    version="0.0.8",
+    version="0.0.9",
     description="A python graph library implemented in Rust",
     long_description=readme(),
     author = "Matthew Treinish",
