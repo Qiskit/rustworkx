767bfd28ed7135588bac38f885dd8b3d09b31e13
Matthew Treinish <mtreinish@kortar.org>
2020-03-06 22:19:18 -0500

    Add s390x wheels
    This commit adds an s390x wheel job to CI that will run at release time
    so we publish wheels for s390x too. We haven't been publishing or
    building wheels for pypi since an issue with the manylinux2014 s390x
    images and installing rust via rustup combined with disk size limits in
    the s390x travis environment preventing use of the offline installer.
    The travis issues have now been resolved so we now have enough space
    to use the offline rust installer. For the next release we'll be
    building s390x wheels.

--- tools/install_rust.sh
+++ tools/install_rust.sh
@@ -1,5 +1,10 @@
 if [ ! -d rust-installer ]; then
     mkdir rust-installer
+    uname -m
+    if [[ `uname -m` == "s390x" ]] ; then
+        export PATH=$PATH:/usr/local/lib
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+    fi
     wget https://sh.rustup.rs -O rust-installer/rustup.sh
     sh rust-installer/rustup.sh --default-toolchain nightly -y
 fi
