638cf2486f36795358101b508077480e09c08289
Matthew Treinish <mtreinish@kortar.org>
2020-06-04 07:19:11 -0400

    Add tests

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setup(
     packages=["retworkx"],
     zip_safe=False,
     python_requires=">=3.5",
-    install_requires=['numpy>=0.16.0'],
+    install_requires=['numpy>=1.16.0'],
 )
---
+++ tests/test_adjacency_matrix.py
@@ -0,0 +1,96 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+import retworkx
+import numpy as np
+
+
+class TestDAGAdjacencyMatrix(unittest.TestCase):
+    def test_single_neighbor(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        dag.add_child(node_a, 'b', {'a': 1})
+        dag.add_child(node_a, 'c', {'a': 2})
+        res = retworkx.dag_adjacency_matrix(dag, lambda x: 1)
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array(
+                [[0.0, 1.0, 1.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]],
+                dtype=np.float64),
+            res))
+
+    def test_float_cast_weight_func(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        dag.add_child(node_a, 'b', 7.0)
+        res = retworkx.dag_adjacency_matrix(dag, lambda x: float(x))
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array([[0.0, 7.0], [0.0, 0.0]]), res))
+
+    def test_multigraph_sum_cast_weight_func(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        node_b = dag.add_child(node_a, 'b', 7.0)
+        dag.add_edge(node_a, node_b, 0.5)
+        res = retworkx.dag_adjacency_matrix(dag, lambda x: float(x))
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array([[0.0, 7.5], [0.0, 0.0]]), res))
+
+    def test_graph_to_dag_adjacency_matrix(self):
+        graph = retworkx.PyGraph()
+        self.assertRaises(TypeError, retworkx.dag_adjacency_matrix, graph)
+
+
+class TestGraphAdjacencyMatrix(unittest.TestCase):
+    def test_single_neighbor(self):
+        graph = retworkx.PyGraph()
+        node_a = graph.add_node('a')
+        node_b = graph.add_node('b')
+        graph.add_edge(node_a, node_b, 'edge_a')
+        node_c = graph.add_node('c')
+        graph.add_edge(node_b, node_c, 'edge_b')
+        res = retworkx.graph_adjacency_matrix(graph, lambda x: 1)
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array(
+                [[0.0, 1.0, 0.0], [1.0, 0.0, 1.0], [0.0, 1.0, 0.0]],
+                dtype=np.float64),
+            res))
+
+    def test_float_cast_weight_func(self):
+        graph = retworkx.PyGraph()
+        node_a = graph.add_node('a')
+        node_b = graph.add_node('b')
+        graph.add_edge(node_a, node_b, 7.0)
+        res = retworkx.graph_adjacency_matrix(graph, lambda x: float(x))
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array([[0.0, 7.0], [7.0, 0.0]]), res))
+
+    def test_multigraph_sum_cast_weight_func(self):
+        graph = retworkx.PyGraph()
+        node_a = graph.add_node('a')
+        node_b = graph.add_node('b')
+        graph.add_edge(node_a, node_b, 7.0)
+        graph.add_edge(node_a, node_b, 0.5)
+        res = retworkx.graph_adjacency_matrix(graph, lambda x: float(x))
+        self.assertIsInstance(res, np.ndarray)
+        self.assertTrue(np.array_equal(
+            np.array([[0.0, 7.5], [7.5, 0.0]]), res))
+
+    def test_dag_to_graph_adjacency_matrix(self):
+        dag = retworkx.PyDAG()
+        self.assertRaises(TypeError, retworkx.graph_adjacency_matrix, dag)
