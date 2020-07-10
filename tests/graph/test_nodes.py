5b0a25920ca10440ddc999a87e78ad75af1a8de4
Matthew Treinish <mtreinish@kortar.org>
2020-07-10 17:41:03 -0400

    Add remove_nodes_from method
    This commit adds a new method to PyGraph and PyDiGraph (which includes
    PyDAG), remove_nodes_from, which enables removing multiple nodes at
    once.
    
    As part of this addition there was an obvious gap in the documentation
    and testing for remove_node() for when it was called with an invalid
    node index. While the behavior was defined it was never called out or
    tested for. This commit took the opportunity to do this since it was
    tangentially related to the new methods being added here.

--- tests/graph/test_nodes.py
+++ tests/graph/test_nodes.py
@@ -40,6 +40,36 @@ class TestNodes(unittest.TestCase):
         self.assertEqual(['a', 'c'], res)
         self.assertEqual([0, 2], graph.node_indexes())
 
+    def test_remove_node_invalid_index(self):
+        graph = retworkx.PyGraph()
+        graph.add_node('a')
+        graph.add_node('b')
+        graph.add_node('c')
+        graph.remove_node(76)
+        res = graph.nodes()
+        self.assertEqual(['a', 'b', 'c'], res)
+        self.assertEqual([0, 1, 2], graph.node_indexes())
+
+    def test_remove_nodes_from(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        node_b = dag.add_child(node_a, 'b', "Edgy")
+        node_c = dag.add_child(node_b, 'c', "Edgy_mk2")
+        dag.remove_nodes_from([node_b, node_c])
+        res = dag.nodes()
+        self.assertEqual(['a'], res)
+        self.assertEqual([0], dag.node_indexes())
+
+    def test_remove_nodes_from_with_invalid_index(self):
+        dag = retworkx.PyDAG()
+        node_a = dag.add_node('a')
+        node_b = dag.add_child(node_a, 'b', "Edgy")
+        node_c = dag.add_child(node_b, 'c', "Edgy_mk2")
+        dag.remove_nodes_from([node_b, node_c, 76])
+        res = dag.nodes()
+        self.assertEqual(['a'], res)
+        self.assertEqual([0], dag.node_indexes())
+
     def test_get_node_data(self):
         graph = retworkx.PyGraph()
         graph.add_node('a')
