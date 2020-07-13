8a2ed6b30b97dbcb303f1ae823a84d69654610f0
Matthew Treinish <mtreinish@kortar.org>
2020-07-13 13:07:17 -0400

    Remove unecessary Python usage from other modules
    Continuing from the previous commit removing unecessary Python usage
    from in_edges() and out_edges() this continues the trend of switching to
    using native rust objects where possible and only use Python when
    necessary and offloading as much to implicit conversion to Python types
    via the PyO3 generated interface.

--- src/lib.rs
+++ src/lib.rs
@@ -103,12 +103,8 @@ fn longest_path(graph: &digraph::PyDiGraph) -> PyResult<Vec<usize>> {
 /// :raises DAGHasCycle: If the input PyDiGraph has a cycle
 #[pyfunction]
 #[text_signature = "(graph, /)"]
-fn dag_longest_path(
-    py: Python,
-    graph: &digraph::PyDiGraph,
-) -> PyResult<PyObject> {
-    let path = longest_path(graph)?;
-    Ok(PyList::new(py, path).into())
+fn dag_longest_path(graph: &digraph::PyDiGraph) -> PyResult<Vec<usize>> {
+    longest_path(graph)
 }
 
 /// Find the length of the longest path in a DAG
@@ -237,21 +233,14 @@ fn is_isomorphic_node_match(
 /// :raises DAGHasCycle: if a cycle is encountered while sorting the graph
 #[pyfunction]
 #[text_signature = "(graph, /)"]
-fn topological_sort(
-    py: Python,
-    graph: &digraph::PyDiGraph,
-) -> PyResult<PyObject> {
+fn topological_sort(graph: &digraph::PyDiGraph) -> PyResult<Vec<usize>> {
     let nodes = match algo::toposort(graph, None) {
         Ok(nodes) => nodes,
         Err(_err) => {
             return Err(DAGHasCycle::py_err("Sort encountered a cycle"))
         }
     };
-    let mut out: Vec<usize> = Vec::new();
-    for node in nodes {
-        out.push(node.index());
-    }
-    Ok(PyList::new(py, out).into())
+    Ok(nodes.iter().map(|node| node.index()).collect())
 }
 
 /// Return successors in a breadth-first-search from a source node.
@@ -303,11 +292,7 @@ fn bfs_successors(
 /// :rtype: list
 #[pyfunction]
 #[text_signature = "(graph, node, /)"]
-fn ancestors(
-    py: Python,
-    graph: &digraph::PyDiGraph,
-    node: usize,
-) -> PyResult<PyObject> {
+fn ancestors(graph: &digraph::PyDiGraph, node: usize) -> HashSet<usize> {
     let index = NodeIndex::new(node);
     let mut out_set: HashSet<usize> = HashSet::new();
     let reverse_graph = Reversed(graph);
@@ -317,7 +302,7 @@ fn ancestors(
         out_set.insert(n_int);
     }
     out_set.remove(&node);
-    Ok(out_set.to_object(py))
+    out_set
 }
 
 /// Return the descendants of a node in a graph.
@@ -333,11 +318,7 @@ fn ancestors(
 /// :returns: A list of node indexes of descendants of provided node.
 /// :rtype: list
 #[pyfunction]
-fn descendants(
-    py: Python,
-    graph: &digraph::PyDiGraph,
-    node: usize,
-) -> PyResult<PyObject> {
+fn descendants(graph: &digraph::PyDiGraph, node: usize) -> HashSet<usize> {
     let index = NodeIndex::new(node);
     let mut out_set: HashSet<usize> = HashSet::new();
     let res = algo::dijkstra(graph, index, None, |_| 1);
@@ -346,7 +327,7 @@ fn descendants(
         out_set.insert(n_int);
     }
     out_set.remove(&node);
-    Ok(out_set.to_object(py))
+    out_set
 }
 
 /// Get the lexicographical topological sorted nodes from the provided DAG
@@ -944,7 +925,7 @@ fn graph_astar_shortest_path(
     goal_fn: PyObject,
     edge_cost_fn: PyObject,
     estimate_cost_fn: PyObject,
-) -> PyResult<PyObject> {
+) -> PyResult<Vec<usize>> {
     let goal_fn_callable = |a: &PyObject| -> PyResult<bool> {
         let res = goal_fn.call1(py, (a,))?;
         let raw = res.to_object(py);
@@ -984,8 +965,7 @@ fn graph_astar_shortest_path(
             ))
         }
     };
-    let out_path: Vec<usize> = path.1.into_iter().map(|x| x.index()).collect();
-    Ok(out_path.to_object(py))
+    Ok(path.1.into_iter().map(|x| x.index()).collect())
 }
 
 /// Compute the A* shortest path for a PyDiGraph
@@ -1017,7 +997,7 @@ fn digraph_astar_shortest_path(
     goal_fn: PyObject,
     edge_cost_fn: PyObject,
     estimate_cost_fn: PyObject,
-) -> PyResult<PyObject> {
+) -> PyResult<Vec<usize>> {
     let goal_fn_callable = |a: &PyObject| -> PyResult<bool> {
         let res = goal_fn.call1(py, (a,))?;
         let raw = res.to_object(py);
@@ -1057,8 +1037,7 @@ fn digraph_astar_shortest_path(
             ))
         }
     };
-    let out_path: Vec<usize> = path.1.into_iter().map(|x| x.index()).collect();
-    Ok(out_path.to_object(py))
+    Ok(path.1.into_iter().map(|x| x.index()).collect())
 }
 
 /// The provided node is invalid.
