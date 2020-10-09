a837312a80535e36ccbc81856ef3a96b7742f707
Matthew Treinish <mtreinish@kortar.org>
2020-10-09 12:19:00 -0400

    Update src/dag_union.rs for Rust Edition 2018
    Since the last edit was pushed on this PR the retworkx repo adopted Rust edition 2018 which changed some of the use semantics. This commit updates dag_union.rs to use these.

--- src/dag_union.rs
+++ src/dag_union.rs
@@ -10,7 +10,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-use digraph::PyDiGraph;
+use crate::digraph::PyDiGraph;
 use petgraph::algo;
 use petgraph::graph::EdgeIndex;
 use pyo3::prelude::*;
