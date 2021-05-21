8b58ce5a8d0fbbd54ceb78072d4a32384637a30d
Matthew Treinish <mtreinish@kortar.org>
2021-05-21 16:08:00 -0400

    Expand test coverage

--- tests/visualization/test_pydot.py
+++ tests/visualization/test_pydot.py
@@ -46,3 +46,99 @@ class TestPyDotDraw(unittest.TestCase):
         image = pydot_draw(graph)
         self.assertIsInstance(image, PIL.Image.Image)
         _save_image(image, "test_pydot_draw.png")
+
+    def test_draw_node_attr_fn(self):
+        graph = retworkx.PyGraph()
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "green",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "red",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_edge(0, 1, dict(label="1", name="1"))
+        image = pydot_draw(graph, lambda node: node)
+        self.assertIsInstance(image, PIL.Image.Image)
+        _save_image(image, "test_pydot_draw_node_attr.png")
+
+    def test_draw_edge_attr_fn(self):
+        graph = retworkx.PyGraph()
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "green",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "red",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_edge(0, 1, dict(label="1", name="1"))
+        image = pydot_draw(graph, lambda node: node, lambda edge: edge)
+        self.assertIsInstance(image, PIL.Image.Image)
+        _save_image(image, "test_pydot_draw_edge_attr.png")
+
+    def test_draw_graph_attr(self):
+        graph = retworkx.PyGraph()
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "green",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_node(
+            {
+                "color": "black",
+                "fillcolor": "red",
+                "label": "a",
+                "style": "filled",
+            }
+        )
+        graph.add_edge(0, 1, dict(label="1", name="1"))
+        graph_attr = {"bgcolor": "red"}
+        image = pydot_draw(
+            graph, lambda node: node, lambda edge: edge, graph_attr
+        )
+        self.assertIsInstance(image, PIL.Image.Image)
+        _save_image(image, "test_pydot_draw_graph_attr.png")
+
+    def test_image_type(self):
+        graph = retworkx.directed_gnp_random_graph(50, 0.8)
+        image = pydot_draw(graph, image_type="jpg")
+        self.assertIsInstance(image, PIL.Image.Image)
+        _save_image(image, "test_pydot_draw_image_type.jpg")
+
+    def test_method(self):
+        graph = retworkx.directed_gnp_random_graph(50, 0.8)
+        image = pydot_draw(graph, method="sfdp")
+        self.assertIsInstance(image, PIL.Image.Image)
+        _save_image(image, "test_pydot_method.png")
+
+    def test_filename(self):
+        graph = retworkx.generators.grid_graph(20, 20)
+        pydot_draw(
+            graph,
+            filename="test_pydot_filename.svg",
+            image_type="svg",
+            method="neato",
+        )
+        self.assertTrue(os.path.isfile("test_pydot_filename.svg"))
+        if not SAVE_IMAGES:
+            self.addCleanup(os.remove, "test_pydot_filename.svg")
