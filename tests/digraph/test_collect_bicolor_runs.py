5f71545e23817c83a0dd7e9b5d5e40f8bd9bb28b
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2021-07-03 21:59:59 -0700

    Add more tests

--- tests/digraph/test_collect_bicolor_runs.py
+++ tests/digraph/test_collect_bicolor_runs.py
@@ -156,3 +156,200 @@ class TestCollectBicolorRuns(unittest.TestCase):
             [["cx", "cz"]],
             retworkx.collect_bicolor_runs(dag, filter_function, color_function),
         )
+
+    def test_two_colors_with_pending(self):
+        """
+        Input:
+        ┌─────────────┐
+        │             │
+        │    q0       │
+        │             │
+        └───┬─────────┘
+            | q0
+            │
+        ┌───▼─────────┐
+        │             │
+        │    h        │
+        │             │
+        └───┬─────────┘
+            | q0
+            │
+        ┌───▼─────────┐                 ┌─────────────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └───┬─────────┘                 └──────┬──────┘
+            │          ┌─────────────┐         │
+        q0  │          │             │         │  q1
+            │          │             │         │
+            └─────────►│     cx      │◄────────┘
+            ┌──────────┤             ├─────────┐
+            │          │             │         │
+        q0  │          └─────────────┘         │  q1
+            │                                  │
+        ┌───▼─────────┐                 ┌──────▼──────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └───┬─────────┘                 └──────┬──────┘
+        q0  │          ┌─────────────┐         │  q1
+            │          │             │         │
+            └─────────►│      cz     │◄────────┘
+             ┌─────────┤             ├─────────┐
+             │         └─────────────┘         │
+         q0  │                                 │ q1
+             │                                 │
+         ┌───▼─────────┐                ┌──────▼──────┐
+         │             │                │             │
+         │    q0       │                │    q1       │
+         │             │                │             │
+         └─────────────┘                └─────────────┘
+                                            | q1
+                                            │
+                                        ┌───▼─────────┐
+                                        │             │
+                                        │    y        │
+                                        │             │
+                                        └─────────────┘
+                                            | q1
+                                            │
+                                        ┌───▼─────────┐
+                                        │             │
+                                        │    q1       │
+                                        │             │
+                                        └─────────────┘
+
+        Expected: [[h, cx, cz, y]]
+        """
+        dag = retworkx.PyDAG()
+        q0_list = []
+        q1_list = []
+        for _ in range(4):
+            q0_list.append(dag.add_node("q0"))
+            q1_list.append(dag.add_node("q1"))
+
+        h_gate = dag.add_node("h")
+        cx_gate = dag.add_node("cx")
+        cz_gate = dag.add_node("cz")
+        y_gate = dag.add_node("y")
+
+        dag.add_edge(q0_list[0], h_gate, "q0")
+        dag.add_edge(h_gate, q0_list[1], "q0")
+        dag.add_edge(q0_list[1], cx_gate, "q0")
+        dag.add_edge(q1_list[0], cx_gate, "q1")
+        dag.add_edge(cx_gate, q0_list[2], "q0")
+        dag.add_edge(cx_gate, q1_list[1], "q1")
+        dag.add_edge(q0_list[2], cz_gate, "q0")
+        dag.add_edge(q1_list[1], cz_gate, "q1")
+        dag.add_edge(cz_gate, q0_list[3], "q0")
+        dag.add_edge(cz_gate, q1_list[2], "q1")
+        dag.add_edge(q1_list[2], y_gate, "q1")
+        dag.add_edge(y_gate, q1_list[3], "q1")
+
+        def filter_function(node):
+            if node in ["cx", "cz", "h", "y"]:
+                return True
+            else:
+                return None
+
+        def color_function(node):
+            if "q" in node:
+                return int(node[1:])
+            else:
+                return -1
+
+        self.assertEqual(
+            [["h", "cx", "cz", "y"]],
+            retworkx.collect_bicolor_runs(dag, filter_function, color_function),
+        )
+
+    def test_two_colors_with_barrier(self):
+        """
+        Input:
+        ┌─────────────┐                 ┌─────────────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └───┬─────────┘                 └──────┬──────┘
+            │          ┌─────────────┐         │
+        q0  │          │             │         │  q1
+            └─────────►│     cx      │◄────────┘
+            ┌──────────┤             ├─────────┐
+        q0  │          └─────────────┘         │  q1
+            │                                  │
+        ┌───▼─────────┐                 ┌──────▼──────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └───┬─────────┘                 └──────┬──────┘
+        q0  │          ┌─────────────┐         │  q1
+            │          │             │         │
+            └─────────►│  barrier    │◄────────┘
+             ┌─────────┤             ├─────────┐
+             │         └─────────────┘         │
+         q0  │                                 │ q1
+             │                                 │
+        ┌────▼────────┐                 ┌──────▼──────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └───┬─────────┘                 └──────┬──────┘
+            │          ┌─────────────┐         │
+        q0  │          │             │         │  q1
+            └─────────►│     cz      │◄────────┘
+            ┌──────────┤             ├─────────┐
+        q0  │          └─────────────┘         │  q1
+            │                                  │
+        ┌───▼─────────┐                 ┌──────▼──────┐
+        │             │                 │             │
+        │    q0       │                 │    q1       │
+        │             │                 │             │
+        └─────────────┘                 └─────────────┘
+
+        Expected: [[cx], [cz]]
+        """
+        dag = retworkx.PyDAG()
+        q0_list = []
+        q1_list = []
+        for _ in range(4):
+            q0_list.append(dag.add_node("q0"))
+            q1_list.append(dag.add_node("q1"))
+
+        cx_gate = dag.add_node("cx")
+        barrier = dag.add_node("barrier")
+        cz_gate = dag.add_node("cz")
+
+        # CX
+        dag.add_edge(q0_list[0], cx_gate, "q0")
+        dag.add_edge(q1_list[0], cx_gate, "q1")
+        dag.add_edge(cx_gate, q0_list[1], "q0")
+        dag.add_edge(cx_gate, q1_list[1], "q1")
+        # Barrier
+        dag.add_edge(q0_list[1], barrier, "q0")
+        dag.add_edge(q1_list[1], barrier, "q1")
+        dag.add_edge(barrier, q0_list[2], "q0")
+        dag.add_edge(barrier, q1_list[2], "q1")
+        # CZ
+        dag.add_edge(q0_list[2], cz_gate, "q0")
+        dag.add_edge(q1_list[2], cz_gate, "q1")
+        dag.add_edge(cz_gate, q0_list[3], "q0")
+        dag.add_edge(cz_gate, q1_list[3], "q1")
+
+        def filter_function(node):
+            if node in ["cx", "cz"]:
+                return True
+            elif node == "barrier":
+                return False
+            else:
+                return None
+
+        def color_function(node):
+            if "q" in node:
+                return int(node[1:])
+            else:
+                return -1
+
+        self.assertEqual(
+            [["cx"], ["cz"]],
+            retworkx.collect_bicolor_runs(dag, filter_function, color_function),
+        )
