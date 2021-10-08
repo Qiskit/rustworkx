de24546b7c2d83dbf92e2cf3135aa4347133dd7a
e-eight <soham@iastate.edu>
2021-10-07 20:03:42 -0500

    Updated tests, API, and added release note

--- tests/generators/test_lollipop.py
+++ tests/generators/test_lollipop.py
@@ -16,18 +16,52 @@ import retworkx
 
 
 class TestLollipopGraph(unittest.TestCase):
-    def test_lollipop_graph(self):
+    def test_lollipop_graph_count(self):
         graph = retworkx.generators.lollipop_graph(17, 3)
         self.assertEqual(len(graph), 20)
         self.assertEqual(len(graph.edges()), 139)
 
-    def test_lollipop_graph_weights(self):
+    def test_lollipop_graph_weights_count(self):
         graph = retworkx.generators.lollipop_graph(
             mesh_weights=list(range(17)), path_weights=list(range(17, 20))
         )
         self.assertEqual(len(graph), 20)
         self.assertEqual(list(range(20)), graph.nodes())
         self.assertEqual(len(graph.edges()), 139)
+
+    def test_lollipop_graph_edge(self):
+        graph = retworkx.generators.lollipop_graph(4, 3)
+        edge_list = graph.edge_list()
+        expected_edge_list = [
+            (0, 1),
+            (0, 2),
+            (0, 3),
+            (1, 2),
+            (1, 3),
+            (2, 3),
+            (3, 4),
+            (4, 5),
+            (5, 6),
+        ]
+        self.assertEqual(edge_list, expected_edge_list)
+
+    def test_lollipop_graph_weights_edge(self):
+        graph = retworkx.generators.lollipop_graph(
+            mesh_weights=list(range(4)), path_weights=list(range(3))
+        )
+        weighted_edge_list = graph.weighted_edge_list()
+        expected_weighted_edge_list = [
+            (0, 1, None),
+            (0, 2, None),
+            (0, 3, None),
+            (1, 2, None),
+            (1, 3, None),
+            (2, 3, None),
+            (3, 4, None),
+            (4, 5, None),
+            (5, 6, None),
+        ]
+        self.assertEqual(weighted_edge_list, expected_weighted_edge_list)
 
     def test_mesh_no_weights_or_num(self):
         with self.assertRaises(IndexError):
