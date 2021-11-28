7bc5e738afa3aade96eb60c175e79edd89e84a09
Ivan Carvalho <ivancarv@student.ubc.ca>
2021-11-27 17:07:35 -0800

    Improve docs

--- src/generators.rs
+++ src/generators.rs
@@ -2240,13 +2240,15 @@ pub fn lollipop_graph(
 ///   
 ///   # Petersen Graph is G(5, 2)
 ///   graph = retworkx.generators.generalized_petersen_graph(5, 2)
-///   layout = retworkx.shell_layout(graph, nlist=[[5, 6, 7, 8, 9], [1, 2, 3, 4, 0]])
+///   layout = retworkx.shell_layout(graph, nlist=[[0, 1, 2, 3, 4],[6, 7, 8, 9, 5]])
 ///   mpl_draw(graph, pos=layout)
+///   
+/// .. jupyter-execute::
 ///   
 ///   # Möbius–Kantor Graph is G(8, 3)
 ///   graph = retworkx.generators.generalized_petersen_graph(8, 3)
 ///   layout = retworkx.shell_layout(
-///     graph, nlist=[[8, 9, 10, 11, 12, 13, 14, 15], [1, 2, 3, 4, 5, 6, 7, 0]]
+///     graph, nlist=[[0, 1, 2, 3, 4, 5, 6, 7], [10, 11, 12, 13, 14, 15, 8, 9]]
 ///   )
 ///   mpl_draw(graph, pos=layout)
 ///
