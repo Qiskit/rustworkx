a7aaf481911dd35732eda8d71be4cfd15536912e
Ivan Carvalho <ivancarv@student.ubc.ca>
2022-05-16 18:10:01 -0700

    Address lint

--- tests/stubs/pydigraph_test.py
+++ tests/stubs/pydigraph_test.py
@@ -10,8 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from typing import List, Tuple
-from retworkx import EdgeList, PyDiGraph, NodeIndices, WeightedEdgeList
+from retworkx import PyDiGraph
 
 import pytest
 
--- tests/stubs/pygraph_test.py
+++ tests/stubs/pygraph_test.py
@@ -10,8 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from typing import List, Tuple
-from retworkx import EdgeList, PyGraph, NodeIndices, WeightedEdgeList
+from retworkx import PyGraph
 
 import pytest
 
