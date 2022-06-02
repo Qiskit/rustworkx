a10e45390983475839eee5b8f06db7df25f155cc
Ivan Carvalho <8753214+IvanIsCoding@users.noreply.github.com>
2022-06-02 05:52:03 -0700

    Add All-Pairs Bellman-Ford (#611)
    * Add all_pairs_bellman_ford to Rust code
    
    * Add Python dispatches and docs
    
    * Add tests
    
    * Tweak release note
    
    * Early return if negative cycle already exists

--- retworkx/__init__.py
+++ retworkx/__init__.py
@@ -2184,3 +2184,95 @@ def _digraph_bellman_ford_shortest_path_lengths(graph, node, edge_cost_fn, goal=
 @bellman_ford_shortest_path_lengths.register(PyGraph)
 def _graph_bellman_ford_shortest_path_lengths(graph, node, edge_cost_fn, goal=None):
     return graph_bellman_ford_shortest_path_lengths(graph, node, edge_cost_fn, goal=goal)
+
+
+@functools.singledispatch
+def all_pairs_bellman_ford_path_lengths(graph, edge_cost_fn):
+    """For each node in the graph, calculates the lengths of the shortest paths to all others.
+
+    This function will generate the shortest path lengths from all nodes in the
+    graph using the Bellman-Ford algorithm. This function is multithreaded and will
+    launch a thread pool with threads equal to the number of CPUs by
+    default. You can tune the number of threads with the ``RAYON_NUM_THREADS``
+    environment variable. For example, setting ``RAYON_NUM_THREADS=4`` would
+    limit the thread pool to 4 threads.
+
+    :param graph: The input graph to use. Can either be a
+        :class:`~retworkx.PyGraph` or :class:`~retworkx.PyDiGraph`
+    :param edge_cost_fn: A callable object that acts as a weight function for
+        an edge. It will accept a single positional argument, the edge's weight
+        object and will return a float which will be used to represent the
+        weight/cost of the edge
+
+    :return: A read-only dictionary of path lengths. The keys are the source
+        node indices and the values are a dict of the target node and the
+        length of the shortest path to that node. For example::
+
+            {
+                0: {1: 2.0, 2: 2.0},
+                1: {2: 1.0},
+                2: {0: 1.0},
+            }
+
+    :rtype: AllPairsPathLengthMapping
+
+    :raises: :class:`~retworkx.NegativeCycle`: when there is a negative cycle and the shortest
+        path is not defined
+    """
+    raise TypeError("Invalid Input Type %s for graph" % type(graph))
+
+
+@all_pairs_bellman_ford_path_lengths.register(PyDiGraph)
+def _digraph_all_pairs_bellman_ford_path_lengths(graph, edge_cost_fn):
+    return digraph_all_pairs_bellman_ford_path_lengths(graph, edge_cost_fn)
+
+
+@all_pairs_bellman_ford_path_lengths.register(PyGraph)
+def _graph_all_pairs_bellman_ford_path_lengths(graph, edge_cost_fn):
+    return graph_all_pairs_bellman_ford_path_lengths(graph, edge_cost_fn)
+
+
+@functools.singledispatch
+def all_pairs_bellman_ford_shortest_paths(graph, edge_cost_fn):
+    """For each node in the graph, finds the shortest paths to all others.
+
+    This function will generate the shortest path from all nodes in the graph
+    using the Bellman-Ford algorithm. This function is multithreaded and will run
+    launch a thread pool with threads equal to the number of CPUs by default.
+    You can tune the number of threads with the ``RAYON_NUM_THREADS``
+    environment variable. For example, setting ``RAYON_NUM_THREADS=4`` would
+    limit the thread pool to 4 threads.
+
+    :param graph: The input graph to use. Can either be a
+        :class:`~retworkx.PyGraph` or :class:`~retworkx.PyDiGraph`
+    :param edge_cost_fn: A callable object that acts as a weight function for
+        an edge. It will accept a single positional argument, the edge's weight
+        object and will return a float which will be used to represent the
+        weight/cost of the edge
+
+    :return: A read-only dictionary of paths. The keys are source node
+        indices and the values are a dict of target node indices and a list
+        of node indices making the path. For example::
+
+            {
+                0: {1: [0, 1],  2: [0, 1, 2]},
+                1: {2: [1, 2]},
+                2: {0: [2, 0]},
+            }
+
+    :rtype: AllPairsPathMapping
+
+    :raises: :class:`~retworkx.NegativeCycle`: when there is a negative cycle and the shortest
+        path is not defined
+    """
+    raise TypeError("Invalid Input Type %s for graph" % type(graph))
+
+
+@all_pairs_bellman_ford_shortest_paths.register(PyDiGraph)
+def _digraph_all_pairs_bellman_ford_shortest_path(graph, edge_cost_fn):
+    return digraph_all_pairs_bellman_ford_shortest_paths(graph, edge_cost_fn)
+
+
+@all_pairs_bellman_ford_shortest_paths.register(PyGraph)
+def _graph_all_pairs_bellman_ford_shortest_path(graph, edge_cost_fn):
+    return graph_all_pairs_bellman_ford_shortest_paths(graph, edge_cost_fn)
