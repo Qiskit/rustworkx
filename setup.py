2ae3ffd63a2ceecf2d5aedde62952d1014719ed9
Matthew Treinish <mtreinish@kortar.org>
2022-07-25 12:43:10 -0400

    Make rustworkx and retworkx independent dependent packages
    This commit splits out the retworkx component from the rustworkx
    package. This makes the new rustworkx package solely contain the
    rustworkx piece and the retworkx piece becomes an independent package
    that depends on rustworkx (and will internally redirect everything to
    rustworkx).
    
    Co-authored-by: Ivan Carvalho <ivancarv@student.ubc.ca>

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ mpl_extras = ['matplotlib>=3.0']
 graphviz_extras = ['pillow>=5.4']
 
 PKG_NAME = os.getenv('RUSTWORKX_PKG_NAME', "rustworkx")
+PKG_VERSION = "0.12.0"
+PKG_PACKAGES = ["rustworkx", "rustworkx.visualization"]
+PKG_INSTALL_REQUIRES = ['numpy>=1.16.0']
 
 retworkx_readme_compat = """# retworkx
 
@@ -35,10 +38,12 @@ will be supported.
 README = readme()
 if PKG_NAME == "retworkx":
     README = retworkx_readme_compat + README
+    PKG_PACKAGES = ["retworkx"]
+    PKG_INSTALL_REQUIRES.append(f"rustworkx=={PKG_VERSION}")
 
 setup(
     name=PKG_NAME,
-    version="0.12.0",
+    version=PKG_VERSION,
     description="A python graph library implemented in Rust",
     long_description=README,
     long_description_content_type='text/markdown',
@@ -69,10 +74,10 @@ setup(
     rust_extensions=[RustExtension("rustworkx.rustworkx", "Cargo.toml",
                                    binding=Binding.PyO3)],
     include_package_data=True,
-    packages=["rustworkx", "rustworkx.visualization", "retworkx"],
+    packages=PKG_PACKAGES,
     zip_safe=False,
     python_requires=">=3.7",
-    install_requires=['numpy>=1.16.0'],
+    install_requires=PKG_INSTALL_REQUIRES,
     extras_require={
         'mpl': mpl_extras,
         'graphviz': graphviz_extras,
