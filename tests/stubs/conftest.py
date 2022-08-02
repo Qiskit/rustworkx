ad83bab59a8bdfc4fb09752b57204658532dac47
Ivan Carvalho <ivancarv@student.ubc.ca>
2022-08-01 17:24:21 -0700

    Ignore no tests found

--- tests/stubs/conftest.py
+++ tests/stubs/conftest.py
@@ -2,5 +2,10 @@
 import sys
 
 collect_ignore_glob = []
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 12):
     collect_ignore_glob.append("*.py")
+
+
+def pytest_sessionfinish(session, exitstatus):
+    if exitstatus == 5:
+        session.exitstatus = 0  # ignore no tests found, it's intentional
