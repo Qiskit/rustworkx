40aac33693308115a4a88ee714dffd8830398880
Matthew Treinish <mtreinish@kortar.org>
2022-09-29 15:36:31 -0400

    Add node weight callback

--- src/dense_subgraph.rs
+++ src/dense_subgraph.rs
@@ -41,7 +41,8 @@ pub fn densest_subgraph<Ty>(
     py: Python,
     graph: &StablePyGraph<Ty>,
     num_nodes: usize,
-    weight_callback: Option<PyObject>,
+    edge_weight_callback: Option<PyObject>,
+    node_weight_callback: Option<PyObject>,
 ) -> PyResult<(StablePyGraph<Ty>, NodeMap)>
 where
     Ty: EdgeType + Sync,
@@ -52,12 +53,17 @@ where
             let res = callback.as_ref(py).call1((source_node, target_node))?;
             res.extract()
         };
-    let mut weight_map: Option<HashMap<[NodeIndex; 2], f64>> = None;
+    let node_callback = |callback: &PyObject, node_index: usize| -> PyResult<f64> {
+        let res = callback.as_ref(py).call1((node_index,))?;
+        res.extract()
+    };
+    let mut edge_weight_map: Option<HashMap<[NodeIndex; 2], f64>> = None;
+    let mut node_weight_map: Option<HashMap<NodeIndex, f64>> = None;
 
-    if weight_callback.is_some() {
+    if edge_weight_callback.is_some() {
         let mut inner_weight_map: HashMap<[NodeIndex; 2], f64> =
             HashMap::with_capacity(graph.edge_count());
-        let callback = weight_callback.as_ref().unwrap();
+        let callback = edge_weight_callback.as_ref().unwrap();
         for edge in graph.edge_references() {
             let source: NodeIndex = edge.source();
             let target: NodeIndex = edge.target();
@@ -67,7 +73,21 @@ where
                 inner_weight_map.insert([target, source], weight);
             }
         }
-        weight_map = Some(inner_weight_map);
+        edge_weight_map = Some(inner_weight_map);
+    }
+    let mut avg_node_error: f64 = 0.;
+    if node_weight_callback.is_some() {
+        let callback = node_weight_callback.as_ref().unwrap();
+        let mut inner_weight_map: HashMap<NodeIndex, f64> =
+            HashMap::with_capacity(graph.node_count());
+        for node in graph.node_indices() {
+            let node_index = node.index();
+            let weight = node_callback(callback, node_index)?;
+            avg_node_error += weight;
+            inner_weight_map.insert(node, weight);
+        }
+        avg_node_error /= graph.node_count() as f64;
+        node_weight_map = Some(inner_weight_map);
     }
     let reduce_identity_fn = || -> SubsetResult {
         SubsetResult {
@@ -79,7 +99,7 @@ where
     };
 
     let reduce_fn = |best: SubsetResult, curr: SubsetResult| -> SubsetResult {
-        if weight_callback.is_some() {
+        if edge_weight_callback.is_some() || node_weight_callback.is_some() {
             if curr.count >= best.count && curr.error <= best.error {
                 curr
             } else {
@@ -119,12 +139,26 @@ where
                     subgraph.push([*node, nbr]);
                 }
             }
-            let error = match &weight_map {
+            let mut error = match &edge_weight_map {
                 Some(map) => {
                     subgraph.iter().map(|edge| map[edge]).sum::<f64>() / subgraph.len() as f64
                 }
                 None => 0.,
             };
+            error *= match &node_weight_map {
+                Some(map) => {
+                    let subgraph_node_error_avg =
+                        bfs_vec.iter().map(|node| map[node]).sum::<f64>() / num_nodes as f64;
+                    let node_error_diff = subgraph_node_error_avg - avg_node_error;
+                    if node_error_diff > 0. {
+                        num_nodes as f64 * node_error_diff
+                    } else {
+                        1.
+                    }
+                }
+                None => 1.,
+            };
+
             Some(SubsetResult {
                 count: connection_count,
                 error,
@@ -157,24 +191,38 @@ where
 ///
 /// :param PyGraph graph: The graph to find densest subgraph in.
 /// :param int num_nodes: The number of nodes in the subgraph to find
-/// :param func weight_callback: An optional callable that if specified will be
+/// :param func edge_weight_callback: An optional callable that if specified will be
 ///     passed the node indices of each edge in the graph and it is expected to
 ///     return a float value. If specified the lowest avg weight for edges in
 ///     a found subgraph will be a criteria for selection in addition to the
 ///     connectivity of the subgraph.
+/// :param func node_weight_callback: An optional callable that if specified will be
+///     passed the node indices of each node in the graph and it is expected to
+///     return a float value. If specified the lowest avg weight for node of
+///     a found subgraph will be a criteria for selection in addition to the
+///     connectivity of the subgraph.//
 /// :returns: A tuple of the subgraph found and a :class:`~.NodeMap` of the
 ///     mapping of node indices in the input ``graph`` to the index in the
 ///     output subgraph.
 /// :rtype: (PyGraph, NodeMap)
 #[pyfunction]
-#[pyo3(text_signature = "(graph, num_nodes, /, weight_callback=None)")]
+#[pyo3(
+    text_signature = "(graph, num_nodes, /, edge_weight_callback=None, node_weight_callback=None)"
+)]
 pub fn graph_densest_subgraph_of_size(
     py: Python,
     graph: &graph::PyGraph,
     num_nodes: usize,
-    weight_callback: Option<PyObject>,
+    edge_weight_callback: Option<PyObject>,
+    node_weight_callback: Option<PyObject>,
 ) -> PyResult<(graph::PyGraph, NodeMap)> {
-    let (inner_graph, node_map) = densest_subgraph(py, &graph.graph, num_nodes, weight_callback)?;
+    let (inner_graph, node_map) = densest_subgraph(
+        py,
+        &graph.graph,
+        num_nodes,
+        edge_weight_callback,
+        node_weight_callback,
+    )?;
     let out_graph = graph::PyGraph {
         graph: inner_graph,
         node_removed: false,
@@ -191,9 +239,14 @@ pub fn graph_densest_subgraph_of_size(
 ///
 /// :param PyDiGraph graph: The graph to find the densest subgraph in.
 /// :param int num_nodes: The number of nodes in the subgraph to find
-/// :param func weight_callback: An optional callable that if specified will be
+/// :param func edge_weight_callback: An optional callable that if specified will be
 ///     passed the node indices of each edge in the graph and it is expected to
 ///     return a float value. If specified the lowest avg weight for edges in
+///     a found subgraph will be a criteria for selection in addition to the
+///     connectivity of the subgraph.
+/// :param func node_weight_callback: An optional callable that if specified will be
+///     passed the node indices of each node in the graph and it is expected to
+///     return a float value. If specified the lowest avg weight for node of
 ///     a found subgraph will be a criteria for selection in addition to the
 ///     connectivity of the subgraph.
 /// :returns: A tuple of the subgraph found and a :class:`~.NodeMap` of the
@@ -201,14 +254,23 @@ pub fn graph_densest_subgraph_of_size(
 ///     output subgraph.
 /// :rtype: (PyDiGraph, NodeMap)
 #[pyfunction]
-#[pyo3(text_signature = "(graph, num_nodes, /, weight_callback=None)")]
+#[pyo3(
+    text_signature = "(graph, num_nodes, /, edge_weight_callback=None, node_weight_callback=None)"
+)]
 pub fn digraph_densest_subgraph_of_size(
     py: Python,
     graph: &digraph::PyDiGraph,
     num_nodes: usize,
-    weight_callback: Option<PyObject>,
+    edge_weight_callback: Option<PyObject>,
+    node_weight_callback: Option<PyObject>,
 ) -> PyResult<(digraph::PyDiGraph, NodeMap)> {
-    let (inner_graph, node_map) = densest_subgraph(py, &graph.graph, num_nodes, weight_callback)?;
+    let (inner_graph, node_map) = densest_subgraph(
+        py,
+        &graph.graph,
+        num_nodes,
+        edge_weight_callback,
+        node_weight_callback,
+    )?;
     let out_graph = digraph::PyDiGraph {
         graph: inner_graph,
         node_removed: false,
