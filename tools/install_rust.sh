9f84f28a51d8337c8f3ba6a0ec19b23bddb16d9a
Matthew Treinish <mtreinish@kortar.org>
2022-10-07 16:31:23 -0400

    Wheel job attempt 2

--- tools/install_rust.sh
+++ tools/install_rust.sh
@@ -1,5 +1,5 @@
 if [ ! -d rust-installer ]; then
     mkdir rust-installer
     wget https://sh.rustup.rs -O rust-installer/rustup.sh
-    sh rust-installer/rustup.sh -y
+    sh rust-installer/rustup.sh -y --default-toolchain 1.48
 fi
