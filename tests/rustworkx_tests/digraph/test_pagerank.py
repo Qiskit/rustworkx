03df46fc0ada5af0ac95c4e05dac83e2069d3049
Ivan Carvalho <ivancarv@student.ubc.ca>
2023-01-23 11:32:47 -0800

    Add more tests

--- tests/rustworkx_tests/digraph/test_pagerank.py
+++ tests/rustworkx_tests/digraph/test_pagerank.py
@@ -41,3 +41,70 @@ class TestPageRank(unittest.TestCase):
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
+
+    def test_multi_digraph(self):
+        rx_graph = rustworkx.PyDiGraph()
+        rx_graph.extend_from_edge_list(
+            [
+                (0, 1),
+                (1, 0),
+                (0, 1),
+                (1, 0),
+                (0, 1),
+                (1, 0),
+                (1, 2),
+                (2, 1),
+                (1, 2),
+                (2, 1),
+                (2, 3),
+                (3, 2),
+                (2, 3),
+                (3, 2),
+            ]
+        )
+        nx_graph = nx.MultiDiGraph(list(rx_graph.edge_list()))
+
+        alpha = 0.9
+        rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha)
+        nx_ranks = nx.pagerank(nx_graph, alpha=alpha)
+
+        for v in rx_graph.node_indices():
+            self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
+
+    def test_no_convergence(self):
+        graph = rustworkx.PyDiGraph()
+        with self.assertRaises(rustworkx.FailedToConverge):
+            rustworkx.pagerank(graph, max_iter=0)
+
+    def test_multi_digraph_versus_weighted(self):
+        multi_graph = rustworkx.PyDiGraph()
+        multi_graph.extend_from_edge_list(
+            [
+                (0, 1),
+                (1, 0),
+                (0, 1),
+                (1, 0),
+                (0, 1),
+                (1, 0),
+                (1, 2),
+                (2, 1),
+                (1, 2),
+                (2, 1),
+                (2, 3),
+                (3, 2),
+                (2, 3),
+                (3, 2),
+            ]
+        )
+
+        weighted_graph = rustworkx.PyDiGraph()
+        weighted_graph.extend_from_weighted_edge_list(
+            [(0, 1, 3), (1, 0, 3), (1, 2, 2), (2, 1, 2), (2, 3, 2), (3, 2, 2)]
+        )
+
+        alpha = 0.85
+        ranks_multi = rustworkx.pagerank(multi_graph, alpha=alpha, weight_fn=lambda _: 1.0)
+        ranks_weight = rustworkx.pagerank(weighted_graph, alpha=alpha, weight_fn=float)
+
+        for v in multi_graph.node_indices():
+            self.assertAlmostEqual(ranks_multi[v], ranks_weight[v], delta=1.0e-4)
