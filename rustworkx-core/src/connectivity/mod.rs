985a010cdfc7f3fb473ff3b5454527f5484aafd0
Edwin Navarro <enavarro@comcast.net>
2023-01-24 09:28:18 -0800

    Move find_cycle to rustworkx-core

--- rustworkx-core/src/connectivity/mod.rs
+++ rustworkx-core/src/connectivity/mod.rs
@@ -16,6 +16,7 @@ mod all_simple_paths;
 mod biconnected;
 mod chain;
 mod conn_components;
+mod find_cycle;
 mod min_cut;
 
 pub use all_simple_paths::all_simple_paths_multiple_targets;
@@ -24,4 +25,5 @@ pub use chain::chain_decomposition;
 pub use conn_components::bfs_undirected;
 pub use conn_components::connected_components;
 pub use conn_components::number_connected_components;
+pub use find_cycle::find_cycle;
 pub use min_cut::stoer_wagner_min_cut;
