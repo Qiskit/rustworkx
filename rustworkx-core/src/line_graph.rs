d475d08c28ddc9cd8a72eaea6ded2fcc2db88693
AlexanderIvrii <alexi@il.ibm.com>
2023-06-08 17:16:50 +0300

    docs

--- rustworkx-core/src/line_graph.rs
+++ rustworkx-core/src/line_graph.rs
@@ -16,6 +16,58 @@ use hashbrown::HashMap;
 use petgraph::data::Create;
 use petgraph::visit::{Data, EdgeCount, EdgeRef, IntoEdges, IntoNodeIdentifiers};
 
+/// Constructs the line graph of an undirected graph.
+///
+/// The line graph `L(G)` of a graph `G` represents the adjacencies between edges of G.
+/// `L(G)` contains a vertex for every edge in `G`, and `L(G)` contains an edge between two
+/// vertices if the corresponding edges in `G` have a vertex in common.
+///
+/// Arguments:
+///
+/// * `input_graph` - The input graph `G`.
+/// * `default_node_weight` - A callable that will return the weight to use
+///     for newly created nodes.
+/// * `default_edge_weight` - A callable that will return the weight object
+///     to use for newly created edges.
+///
+/// Returns the constructed line graph `L(G)`, and the map from the edges of `L(G)` to
+/// the vertices of `G`.
+///
+/// # Example
+/// ```rust
+/// use crate::line_graph::line_graph;
+/// use crate::petgraph::visit::EdgeRef;
+/// use crate::petgraph::Graph;
+/// use hashbrown::HashMap;
+/// use petgraph::graph::{EdgeIndex, NodeIndex};
+/// use petgraph::Undirected;
+///
+/// let input_graph =
+///   Graph::<(), (), Undirected>::from_edges(&[(0, 1), (0, 2), (1, 2), (0, 3)]);
+///
+/// let (output_graph, output_edge_map): (
+///     petgraph::graph::UnGraph<(), ()>,
+///     HashMap<petgraph::prelude::EdgeIndex, petgraph::prelude::NodeIndex>,
+/// ) = line_graph(&input_graph, || (), || ());
+///
+/// let output_edge_list = output_graph
+///     .edge_references()
+///     .map(|edge| (edge.source().index(), edge.target().index()))
+///     .collect::<Vec<(usize, usize)>>();///
+///
+/// let expected_edge_list = vec![(3, 1), (3, 0), (1, 0), (2, 0), (2, 1)];
+/// let expected_edge_map: HashMap<EdgeIndex, NodeIndex> = [
+///     (EdgeIndex::new(0), NodeIndex::new(0)),
+///     (EdgeIndex::new(1), NodeIndex::new(1)),
+///     (EdgeIndex::new(2), NodeIndex::new(2)),
+///     (EdgeIndex::new(3), NodeIndex::new(3)),
+/// ]
+/// .into_iter()
+/// .collect();
+///
+/// assert_eq!(output_edge_list, expected_edge_list);
+/// assert_eq!(output_edge_map, expected_edge_map);
+/// ```
 pub fn line_graph<K, G, T, F, H, M>(
     input_graph: K,
     mut default_node_weight: F,
