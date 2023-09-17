6cabf186262f5400cf91deea5d7ca5e2cd062ef5
AlexanderIvrii <alexi@il.ibm.com>
2023-09-17 21:21:05 +0300

    python tests and wrapper function

--- src/coloring.rs
+++ src/coloring.rs
@@ -12,7 +12,7 @@
 
 use crate::{graph, StablePyGraph};
 use hashbrown::HashSet;
-use rustworkx_core::coloring::{greedy_edge_color, greedy_node_color, mg_color};
+use rustworkx_core::coloring::{greedy_edge_color, greedy_node_color, misra_gries_edge_color};
 
 use petgraph::graph::{EdgeIndex, NodeIndex};
 use petgraph::visit::EdgeRef;
@@ -66,212 +66,6 @@ pub fn graph_greedy_color(py: Python, graph: &graph::PyGraph) -> PyResult<PyObje
     Ok(out_dict.into())
 }
 
-struct MisraGriesAlgorithm<'a> {
-    graph: &'a StablePyGraph<Undirected>,
-    colors: Vec<Option<usize>>,
-}
-
-impl<'a> MisraGriesAlgorithm<'a> {
-    pub fn new(graph: &'a StablePyGraph<Undirected>) -> Self {
-        let colors = vec![None; graph.edge_count()];
-        MisraGriesAlgorithm { graph, colors }
-    }
-
-    // Computes colors used at node u
-    fn get_used_colors(&self, u: NodeIndex) -> HashSet<usize> {
-        let used_colors: HashSet<usize> = self
-            .graph
-            .edges(u)
-            .filter_map(|edge| self.colors[edge.id().index()])
-            .collect();
-        used_colors
-    }
-
-    // Returns the smallest free (aka unused) color at node u
-    fn get_free_color(&self, u: NodeIndex) -> usize {
-        let used_colors = self.get_used_colors(u);
-        let free_color: usize = (0..)
-            .position(|color| !used_colors.contains(&color))
-            .unwrap();
-        free_color
-    }
-
-    // Returns if color c is free at node u
-    fn is_free_color(&self, u: NodeIndex, c: usize) -> bool {
-        let used_colors = self.get_used_colors(u);
-        !used_colors.contains(&c)
-    }
-
-    // Returns the maximal fan on edge ee = (u, v) at u
-    fn get_maximal_fan(
-        &self,
-        ee: EdgeIndex,
-        u: NodeIndex,
-        v: NodeIndex,
-    ) -> Vec<(EdgeIndex, NodeIndex)> {
-        let mut fan: Vec<(EdgeIndex, NodeIndex)> = Vec::new();
-        fan.push((ee, v));
-
-        let mut neighbors: Vec<(EdgeIndex, NodeIndex)> = self
-            .graph
-            .edges(u)
-            .map(|edge| (edge.id(), edge.target()))
-            .collect();
-
-        let mut last_node = v;
-        let position_v = neighbors.iter().position(|x| x.1 == v).unwrap();
-        neighbors.remove(position_v);
-
-        let mut fan_extended: bool = true;
-        while fan_extended {
-            fan_extended = false;
-
-            for (edge_index, z) in &neighbors {
-                if let Some(color) = self.colors[edge_index.index()] {
-                    if self.is_free_color(last_node, color) {
-                        fan_extended = true;
-                        last_node = *z;
-                        fan.push((*edge_index, *z));
-                        let position_z = neighbors.iter().position(|x| x.1 == *z).unwrap();
-                        neighbors.remove(position_z);
-                        break;
-                    }
-                }
-            }
-
-            // for (position, (edge_index, z)) in neighbors.iter().enumerate() {
-            //     if let Some(color) = self.colors.get(edge_index) {
-            //         if self.is_free_color(last_node, *color) {
-            //             fan_extended = true;
-            //             last_node = *z;
-            //             fan.push((*edge_index, *z));
-            //             neighbors.remove(position);
-            //             break;
-            //         }
-            //     }
-            // }
-        }
-
-        fan
-    }
-
-    fn flip_color(&self, c: usize, d: usize, e: usize) -> usize {
-        if e == c {
-            d
-        } else {
-            c
-        }
-    }
-
-    // Returns the longest path starting at node u with alternating colors c, d, c, d, c, etc.
-    fn get_cdu_path(&self, u: NodeIndex, c: usize, d: usize) -> Vec<(EdgeIndex, usize)> {
-        let mut path: Vec<(EdgeIndex, usize)> = Vec::new();
-        let mut cur_node: NodeIndex = u;
-        let mut cur_color = c;
-        let mut path_extended = true;
-
-        while path_extended {
-            path_extended = false;
-            for edge in self.graph.edges(cur_node) {
-                if let Some(color) = self.colors[edge.id().index()] {
-                    if color == cur_color {
-                        path_extended = true;
-                        path.push((edge.id(), cur_color));
-                        cur_node = edge.target();
-                        cur_color = self.flip_color(c, d, cur_color);
-                        break;
-                    }
-                }
-            }
-        }
-        path
-    }
-
-    fn check_coloring(&self) -> bool {
-        for edge in self.graph.edge_references() {
-            match self.colors[edge.id().index()] {
-                Some(_color) => (),
-                None => {
-                    println!("Problem edge {:?} has no color assigned", edge);
-                    return false;
-                }
-            }
-        }
-
-        let mut max_color = 0;
-        for node in self.graph.node_indices() {
-            let mut used_colors: HashSet<usize> = HashSet::new();
-            let mut num_edges = 0;
-            for edge in self.graph.edges(node) {
-                num_edges += 1;
-                match self.colors[edge.id().index()] {
-                    Some(color) => {
-                        used_colors.insert(color);
-                        if max_color < color {
-                            max_color = color;
-                        }
-                    }
-                    None => {
-                        println!("Problem: edge {:?} has no color assigned", edge);
-                        return false;
-                    }
-                }
-            }
-            if used_colors.len() < num_edges {
-                println!("Problem: node {:?} does not have enough colors", node);
-                return false;
-            }
-        }
-
-        println!("Coloring is OK, max_color = {}", max_color);
-        true
-    }
-
-    pub fn run_algorithm(&mut self) -> &Vec<Option<usize>> {
-        println!("run_algorithm 1!");
-        for edge in self.graph.edge_references() {
-            let u: NodeIndex = edge.source();
-            let v: NodeIndex = edge.target();
-            let fan = self.get_maximal_fan(edge.id(), u, v);
-            let c = self.get_free_color(u);
-            let d = self.get_free_color(fan.last().unwrap().1);
-
-            // find cdu-path
-            let cdu_path = self.get_cdu_path(u, d, c);
-
-            // invert colors on cdu-path
-            for (edge_index, color) in cdu_path {
-                let flipped_color = self.flip_color(c, d, color);
-                self.colors[edge_index.index()] = Some(flipped_color);
-            }
-
-            // find sub-fan fan[0..w] such that d is free on fan[w]
-            let mut w = 0;
-            for (i, (_, z)) in fan.iter().enumerate() {
-                if self.is_free_color(*z, d) {
-                    w = i;
-                    break;
-                }
-            }
-
-            // rotate fan
-            for i in 1..w + 1 {
-                let next_color = self.colors[fan[i].0.index()].unwrap();
-                let edge_id = fan[i - 1].0;
-                self.colors[edge_id.index()] = Some(next_color);
-            }
-
-            // fill additional color
-            let edge_id = fan[w].0;
-            self.colors[edge_id.index()] = Some(d);
-        }
-
-        // self.check_coloring();
-
-        &self.colors
-    }
-}
-
 /// Color edges of a :class:`~.PyGraph` object using a greedy approach.
 ///
 /// This function works by greedily coloring the line graph of the given graph.
@@ -300,17 +94,30 @@ pub fn graph_greedy_edge_color(py: Python, graph: &graph::PyGraph) -> PyResult<P
     Ok(out_dict.into())
 }
 
+/// Color edges of a :class:`~.PyGraph` object using a greedy approach.
+///
+/// This function works by greedily coloring the line graph of the given graph.
+///
+/// :param PyGraph: The input PyGraph object to edge-color
+///
+/// :returns: A dictionary where keys are edge indices and the value is the color
+/// :rtype: dict
+///
+/// .. jupyter-execute::
+///
+///   import rustworkx as rx
+///
+///   graph = rx.generators.cycle_graph(7)
+///   edge_colors = rx.graph_misra_gries_edge_color(graph)
+///   assert edge_colors == {0: 0, 1: 1, 2: 2, 3: 0, 4: 1, 5: 0, 6: 2}
+///
 #[pyfunction]
 #[pyo3(text_signature = "(graph, /)")]
 pub fn graph_misra_gries_edge_color(py: Python, graph: &graph::PyGraph) -> PyResult<PyObject> {
-    let mut mg = MisraGriesAlgorithm::new(&graph.graph);
-
-    let colors = mg.run_algorithm();
-
+    let colors = misra_gries_edge_color(&graph.graph);
     let out_dict = PyDict::new(py);
-    for (edge, color) in colors.iter().enumerate() {
-        out_dict.set_item(edge, color.unwrap())?;
+    for (node, color) in colors {
+        out_dict.set_item(node.index(), color)?;
     }
     Ok(out_dict.into())
 }
-
--- tests/rustworkx_tests/graph/test_coloring.py
+++ tests/rustworkx_tests/graph/test_coloring.py
@@ -46,56 +46,6 @@ class TestGraphColoring(unittest.TestCase):
         self.assertEqual({0: 0, 1: 1, 2: 1}, res)
 
 
-class TestMisraGriesColoring(unittest.TestCase):
-
-    def test_simple_graph(self):
-        graph = rustworkx.PyGraph()
-        node0 = graph.add_node(0)
-        node1 = graph.add_node(1)
-        node2 = graph.add_node(2)
-        node3 = graph.add_node(3)
-
-        graph.add_edge(node0, node1, 1)
-        graph.add_edge(node0, node2, 1)
-        graph.add_edge(node1, node2, 1)
-        graph.add_edge(node2, node3, 1)
-
-        print("======================")
-        res = rustworkx.graph_misra_gries_edge_color(graph)
-        print(f"{res = }")
-
-
-    def test_grid(self):
-        graph = rustworkx.generators.grid_graph(10, 10);
-
-        print("======================")
-        res = rustworkx.graph_misra_gries_edge_color(graph)
-        print(f"{res = }")
-
-
-    def test_heavy_hex(self):
-        graph = rustworkx.generators.heavy_hex_graph(7);
-
-        print("======================")
-        res = rustworkx.graph_misra_gries_edge_color(graph)
-        print(f"{res = }")
-
-    def test_barbell(self):
-
-        # graph = rustworkx.generators.barbell_graph(4, 3)
-        graph = rustworkx.generators.complete_graph(10)
-
-        print("======================")
-        res = rustworkx.graph_misra_gries_edge_color(graph)
-        print(f"{res = }")
-
-    def test_path_graph(self):
-        print("HERE")
-        graph = rustworkx.generators.path_graph(15)
-        res = rustworkx.graph_misra_gries_edge_color(graph)
-        print(f"{res = }")
-
-
 class TestGraphEdgeColoring(unittest.TestCase):
     def test_graph(self):
         graph = rustworkx.PyGraph()
@@ -156,3 +106,82 @@ class TestGraphEdgeColoring(unittest.TestCase):
         graph = rustworkx.generators.cycle_graph(7)
         edge_colors = rustworkx.graph_greedy_edge_color(graph)
         self.assertEqual({0: 0, 1: 1, 2: 0, 3: 1, 4: 0, 5: 1, 6: 2}, edge_colors)
+
+
+class TestMisraGriesColoring(unittest.TestCase):
+    def test_simple_graph(self):
+        graph = rustworkx.PyGraph()
+        node0 = graph.add_node(0)
+        node1 = graph.add_node(1)
+        node2 = graph.add_node(2)
+        node3 = graph.add_node(3)
+
+        graph.add_edge(node0, node1, 1)
+        graph.add_edge(node0, node2, 1)
+        graph.add_edge(node1, node2, 1)
+        graph.add_edge(node2, node3, 1)
+
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        self.assertEqual({0: 2, 1: 1, 2: 0, 3: 2}, edge_colors)
+
+    def test_graph_with_holes(self):
+        """Graph with missing node and edge indices."""
+        graph = rustworkx.PyGraph()
+        node_a = graph.add_node("a")
+        node_b = graph.add_node("b")
+        node_c = graph.add_node("c")
+        node_d = graph.add_node("d")
+        node_e = graph.add_node("e")
+        graph.add_edge(node_a, node_b, 1)
+        graph.add_edge(node_b, node_c, 1)
+        graph.add_edge(node_c, node_d, 1)
+        graph.add_edge(node_d, node_e, 1)
+        graph.remove_node(node_c)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        self.assertEqual({0: 0, 3: 0}, edge_colors)
+
+    def test_graph_without_edges(self):
+        graph = rustworkx.PyGraph()
+        graph.add_node("a")
+        graph.add_node("b")
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        self.assertEqual({}, edge_colors)
+
+    def test_graph_multiple_edges(self):
+        """Graph with multiple edges between two nodes."""
+        graph = rustworkx.PyGraph()
+        node_a = graph.add_node("a")
+        node_b = graph.add_node("b")
+        graph.add_edge(node_a, node_b, 1)
+        graph.add_edge(node_a, node_b, 1)
+        graph.add_edge(node_a, node_b, 1)
+        graph.add_edge(node_a, node_b, 1)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        self.assertEqual({0: 0, 1: 1, 2: 2, 3: 3}, edge_colors)
+
+    def test_cycle_graph(self):
+        """Test on a small cycle graph with an odd number of virtices."""
+        graph = rustworkx.generators.cycle_graph(7)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        assert edge_colors == {0: 0, 1: 1, 2: 2, 3: 0, 4: 1, 5: 0, 6: 2}
+
+    def test_grid(self):
+        """Test that Misra-Gries colors the grid with at most 5 colors (max degree + 1)."""
+        graph = rustworkx.generators.grid_graph(10, 10)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        num_colors = max(edge_colors.values()) + 1
+        self.assertLessEqual(num_colors, 5)
+
+    def test_heavy_hex(self):
+        """Test that Misra-Gries colors the heavy hex with at most 4 colors (max degree + 1)."""
+        graph = rustworkx.generators.heavy_hex_graph(9)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        num_colors = max(edge_colors.values()) + 1
+        self.assertLessEqual(num_colors, 4)
+
+    def test_complete_graph(self):
+        """Test that Misra-Gries colors the complete graph with at most n+1 colors."""
+        graph = rustworkx.generators.complete_graph(10)
+        edge_colors = rustworkx.graph_misra_gries_edge_color(graph)
+        num_colors = max(edge_colors.values()) + 1
+        self.assertLessEqual(num_colors, 11)
