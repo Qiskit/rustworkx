f7c41222ce723aad42a6ff3e2aab5b5581077af6
Ivan Carvalho <ivancarv@student.ubc.ca>
2023-12-07 00:11:27 -0500

    Add code to build release versions

---
+++ tools/build_documentation_release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2018, 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# Script for building docs
+# It builds the latest docs and pulls the historical stable docs
+# from the git repository
+set -e
+
+# Build the documentation.
+tox -edocs
+
+# Extract the release version from Cargo.toml
+VERSION=$(grep -Po -m1 'version = "\K[0-9]+\.[0-9]+' Cargo.toml)
+
+TMP_DIR=$(mktemp -d)
+git clone --depth 1 --branch gh-pages https://github.com/Qiskit/rustworkx.git $TMP_DIR
+mkdir -p $TMP_DIR/stable/$VERSION
+cp -r docs/build/html/* $TMP_DIR/stable/$VERSION/*
+mkdir -p release_docs
+cp -r $TMP_DIR/stable/$VERSION/* release_docs/
+
+# Delete the temporary directory
+rm -rf $TMP_DIR
