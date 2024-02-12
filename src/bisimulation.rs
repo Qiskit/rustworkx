1ab1908ae01525fad03ee6383b3bd2e70516e7cc
sander-hergarten <mail@sander-hergarten.de>
2024-02-12 15:11:05 +0100

    rewrote initialization

--- src/bisimulation.rs
+++ src/bisimulation.rs
@@ -1,5 +1,4 @@
 use std::cell::RefCell;
-use std::collections::HashSet;
 use std::rc::Rc;
 
 use pyo3::prelude::*;
@@ -13,88 +12,88 @@ use petgraph::graph;
 use petgraph::Direction::{Incoming, Outgoing};
 
 type Block = Vec<graph::NodeIndex>;
-type Partition = HashSet<Block>;
-
-type CoarseBlock = Vec<graph::NodeIndex>;
-type CoarsePartition = Vec<Rc<CoarseBlock>>;
-
-type SubBlock = Block;
 
 type Image = HashMap<graph::NodeIndex, Vec<graph::NodeIndex>>;
 type Counterimage = Image;
 type CounterimageGrouped = HashMap<Rc<Block>, Rc<SubBlock>>;
 
-type MapFineBlockCoarseBlock = HashMap<Rc<Block>, Rc<Block>>;
-type CoarseBlockMakeup = HashMap<Rc<CoarseBlock>, Vec<Rc<Block>>>;
-type NodeToBlockVec = Vec<Rc<Block>>;
+struct FineBlock {
+    values: Block,
+    coarse_block_that_supersets_self: Rc<CoarseBlock>,
+}
+
+struct CoarseBlock {
+    values: Block,
+    fine_blocks_that_are_subsets_of_self: RefCell<Vec<Rc<RefCell<FineBlock>>>>,
+}
+
+type NodeToBlockVec = Vec<Rc<RefCell<FineBlock>>>;
+
+type CoarsePartition = Vec<Rc<CoarseBlock>>;
 
 fn initialization(
     graph: &StablePyGraph<Directed>,
 ) -> (
-    Partition,
+    (Rc<RefCell<FineBlock>>, Rc<RefCell<FineBlock>>),
     CoarsePartition,
     NodeToBlockVec,
-    CoarseBlockMakeup,
-    MapFineBlockCoarseBlock,
 ) {
-    let mut graph_node_indices: Block = graph.node_indices().clone().into_iter().collect();
-    graph_node_indices.sort();
+    let graph_node_indices: Block = graph.node_indices().clone().into_iter().collect();
 
-    let mut leaf_node_indices_pointer: RefCell<Block> = RefCell::new(Vec::new());
-    let mut non_leaf_node_indices_pointer: RefCell<Block> = RefCell::new(Vec::new());
+    let coarse_initial_block_pointer: Rc<CoarseBlock> = {
+        let coarse_initial_block = CoarseBlock {
+            values: graph_node_indices.clone(),
+            fine_blocks_that_are_subsets_of_self: RefCell::new(vec![]),
+        };
 
-    let mut node_to_block_vec = vec![
-        Rc::clone(&non_leaf_node_indices_pointer);
-        graph_node_indices.last().unwrap().clone().index() as usize
-    ];
+        Rc::new(coarse_initial_block)
+    };
 
-    let (coarse_block_pointer, coarse_partition_pointer): (Rc<CoarseBlock>, Rc<CoarsePartition>) = {
-        let coarse_block = Rc::new(graph_node_indices);
+    let (leaf_node_block_pointer, non_leaf_node_block_pointer) = {
+        let (leaf_node_indices, non_leaf_node_indices): (Block, Block) = graph_node_indices
+            .clone()
+            .into_iter()
+            .partition(|x| graph.neighbors_directed(*x, Outgoing).count() == 0);
+
+        let leaf_node_block = FineBlock {
+            values: leaf_node_indices,
+            coarse_block_that_supersets_self: Rc::clone(&coarse_initial_block_pointer),
+        };
+        let non_leaf_node_block = FineBlock {
+            values: non_leaf_node_indices,
+            coarse_block_that_supersets_self: Rc::clone(&coarse_initial_block_pointer),
+        };
 
         (
-            Rc::clone(&coarse_block),
-            Rc::new(vec![Rc::clone(&coarse_block)]),
+            Rc::new(RefCell::new(leaf_node_block)),
+            Rc::new(RefCell::new(non_leaf_node_block)),
         )
     };
 
-    let fine_block_to_coarse: MapFineBlockCoarseBlock = HashMap::from([
-        (
-            Rc::clone(&non_leaf_node_indices_pointer),
-            Rc::clone(&coarse_block_pointer),
-        ),
-        (
-            Rc::clone(&leaf_node_indices_pointer),
-            Rc::clone(&coarse_block_pointer),
-        ),
-    ]);
+    coarse_initial_block_pointer
+        .fine_blocks_that_are_subsets_of_self
+        .borrow_mut()
+        .extend([
+            Rc::clone(&leaf_node_block_pointer),
+            Rc::clone(&non_leaf_node_block_pointer),
+        ]);
 
-    let coarse_block_makeup: CoarseBlockMakeup = HashMap::from([(
-        Rc::clone(&coarse_block_pointer),
+    let node_to_block_vec = leaf_node_block_pointer.borrow().values.clone().iter().fold(
         vec![
-            Rc::clone(&non_leaf_node_indices_pointer),
-            Rc::clone(&leaf_node_indices_pointer),
+            Rc::clone(&non_leaf_node_block_pointer);
+            graph_node_indices.last().unwrap().clone().index() as usize
         ],
-    )]);
-
-    graph.node_indices().into_iter().for_each(|x| {
-        match graph.neighbors_directed(x, Outgoing).count() == 0 {
-            true => {
-                node_to_block_vec[x.index()] = Rc::clone(&leaf_node_indices_pointer);
-                leaf_node_indices_pointer.push(x);
-            }
-            false => {
-                non_leaf_node_indices_pointer.push(x);
-            }
-        }
-    });
-
-    return (
-        HashSet::from([*leaf_node_indices_pointer, *non_leaf_node_indices_pointer]),
-        *coarse_partition_pointer,
-        node_to_block_vec,
-        coarse_block_makeup,
-        fine_block_to_coarse,
+        |mut accumulator, value| {
+            accumulator[value.index()] = Rc::clone(&leaf_node_block_pointer);
+            accumulator
+        },
     );
+
+    (
+        (leaf_node_block_pointer, non_leaf_node_block_pointer),
+        vec![coarse_initial_block_pointer],
+        node_to_block_vec,
+    )
 }
 
 fn build_counterimage(graph: &StablePyGraph<Directed>, block: Block) -> Counterimage {
@@ -179,46 +178,56 @@ fn repartition(
     }
 }
 
-fn find_smallest_component_block_and_copy(
-    coarse_block: CoarseBlock,
-    coarse_block_makeup: Vec<Rc<Block>>,
-) -> Block {
+fn find_smallest_component_block_and_copy(coarse_block_makeup: Vec<Rc<Block>>) -> Block {
     let smallest_component_block_pointer = coarse_block_makeup
         .iter()
         .min_by(|x, y| x.len().cmp(&y.len()))
         .unwrap();
-    return (**smallest_component_block_pointer).clone();
+
+    (**smallest_component_block_pointer).clone()
 }
 
 fn maximum_bisimulation(graph: &StablePyGraph<Directed>) -> Partition {
-    let (
-        mut partition,
-        initial_coarse_partition,
-        mut node_to_block_vec,
-        mut coarse_block_makeup,
-        mut fine_partition_to_coarse,
-    ) = initialization(graph);
+    let (fine_block_tuple, initial_coarse_partition, mut node_to_block_vec) = initialization(graph);
 
     let mut queue: CoarsePartition = initial_coarse_partition;
 
     while !queue.is_empty() {
         let (smaller_component, simple_splitter_block) = {
             let splitter_block = queue.pop().unwrap();
-            let coarse_blocks_in_splitter_block =
-                *coarse_block_makeup.get(&splitter_block).unwrap();
-            if !(coarse_blocks_in_splitter_block.len() == 2) {
+            let fine_blocks_in_splitter_block = splitter_block
+                .fine_blocks_that_are_subsets_of_self
+                .borrow()
+                .clone();
+
+            if !(fine_blocks_in_splitter_block.len() == 2) {
                 queue.push(splitter_block);
             }
 
-            let smaller_component = find_smallest_component_block_and_copy(
-                *splitter_block,
-                coarse_blocks_in_splitter_block,
+            let smaller_component = fine_blocks_in_splitter_block
+                .iter()
+                .min_by(|x, y| x.borrow().values.len().cmp(&y.borrow().values.len()))
+                .unwrap()
+                .clone();
+            let simple_splitter_block_subsets = RefCell::new(
+                fine_blocks_in_splitter_block
+                    .iter()
+                    .filter(|x| x.borrow().values != smaller_component.borrow().values)
+                    .collect(),
             );
 
-            let simple_splitter_block: CoarseBlock = splitter_block
-                .into_iter()
-                .filter(|x| !smaller_component.contains(x))
+            let simple_splitter_block_values: Block = splitter_block
+                .values
+                .clone()
+                .iter()
+                .filter(|x| !smaller_component.borrow().values.contains(x))
+                .map(|x| *x)
                 .collect();
+
+            let simple_splitter_block = CoarseBlock {
+                values: simple_splitter_block_values,
+                fine_blocks_that_are_subsets_of_self: simple_splitter_block_subsets,
+            };
 
             (smaller_component, simple_splitter_block)
         };
