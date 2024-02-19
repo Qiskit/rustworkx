765207e4eea5d40d3b047f2c334c8f4c384506a3
Sebastian Brandhofer <148463728+sbrandhsn@users.noreply.github.com>
2024-02-19 19:00:35 +0100

    interface ok

--- src/connectivity/mod.rs
+++ src/connectivity/mod.rs
@@ -14,6 +14,7 @@
 
 mod all_pairs_all_simple_paths;
 mod johnson_simple_cycles;
+mod subgraphs;
 
 use super::{
     digraph, get_edge_iter_with_weights, graph, score, weight_callable, InvalidNode, NullGraph,
@@ -41,6 +42,7 @@ use crate::{EdgeType, StablePyGraph};
 
 use rustworkx_core::coloring::two_color;
 use rustworkx_core::connectivity;
+use crate::graph::PyGraph;
 
 /// Return a list of cycles which form a basis for cycles of a given PyGraph
 ///
@@ -658,6 +660,33 @@ pub fn digraph_all_pairs_all_simple_paths(
         cutoff,
     ))
 }
+
+/// Return all the connected subgraphs with <= k nodes
+///
+///
+/// :param PyGraph graph: The graph to find all connected subgraphs in
+/// :param int k: The maximum number of nodes in a returned connected subgraph.
+///
+/// :returns: A set of graphs
+///
+/// :raises ValueError: If ``k`` is larger than the number of nodes in ``graph``
+#[pyfunction]
+#[pyo3(text_signature = "(graph, k, /)")]
+pub fn connected_subgraphs(
+    graph: &PyGraph,
+    k: usize
+) -> PyResult<Vec<Vec<usize>>> {
+    if k > graph.node_count() {
+        return Err(PyValueError::new_err("Value for k must be < node count in input graph"));
+    }
+
+    Ok(subgraphs::k_connected_subgraphs(
+        &graph.graph,
+        k,
+    ))
+}
+
+
 
 /// Return all the simple paths between all pairs of nodes in the graph
 ///
--- tests/graph/test_all_connected_subgraphs.py
+++ tests/graph/test_all_connected_subgraphs.py
@@ -101,68 +101,63 @@ class TestGraphAllConnectedSubgraphs(unittest.TestCase):
         for node_list in self.expected_subgraph_nodes:
             graph = rustworkx.PyGraph()
             graph.add_nodes_from(node_list)
+            graph.nodes()
             node_index_map = {n: i for i, n in enumerate(node_list)}
             for e in self.edges:
                 if e[0] in graph.nodes() and e[1] in graph.nodes():
                     graph.add_edge(node_index_map[e[0]], node_index_map[e[1]], None)
-            self.expected_subgraphs.setdefault(len(node_list), set()).add(graph)
+            self.expected_subgraphs.setdefault(len(node_list), list()).append(graph.nodes())
 
         for n in self.nodes:
             g = rustworkx.PyGraph()
             g.add_node(n)
-            self.expected_subgraphs.setdefault(1, set()).add(g)
+            self.expected_subgraphs.setdefault(1, list()).append(g.nodes())
 
         for e in enumerate(self.edges):
             g = rustworkx.PyGraph()
             g.add_nodes_from(e)
             node_index_map = {n: i for i, n in enumerate(e)}
             g.add_edge(node_index_map[e[0]], node_index_map[e[1]], None)
-            self.expected_subgraphs.setdefault(2, set()).add(g)
+            self.expected_subgraphs.setdefault(2, list()).append(g.nodes())
 
     def test_empty_graph(self):
         graph = rustworkx.PyGraph()
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 3)
-        expected = {rustworkx.PyGraph()}
+        subgraphs = rustworkx.connected_subgraphs(graph, 0)
+        expected = []
         self.assertEqual(subgraphs, expected)
 
     def test_empty_graph_2(self):
         graph = rustworkx.PyGraph()
         graph.add_nodes_from(self.nodes)
         graph.add_edges_from_no_data(self.edges)
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 0)
-        expected = {rustworkx.PyGraph()}
+        subgraphs = rustworkx.connected_subgraphs(graph, 0)
+        expected = []
         self.assertEqual(subgraphs, expected)
 
     def test_size_one_subgraphs(self):
         graph = rustworkx.PyGraph()
         graph.add_nodes_from(self.nodes)
         graph.add_edges_from_no_data(self.edges)
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 1)
+        subgraphs = rustworkx.connected_subgraphs(graph, 1)
         self.assertEqual(subgraphs, self.expected_subgraphs[1])
 
     def test_size_two_subgraphs(self):
         graph = rustworkx.PyGraph()
         graph.add_nodes_from(self.nodes)
         graph.add_edges_from_no_data(self.edges)
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 2)
+        subgraphs = rustworkx.connected_subgraphs(graph, 2)
         self.assertEqual(subgraphs, self.expected_subgraphs[1]+self.expected_subgraphs[2])
 
     def test_size_three_subgraphs(self):
         graph = rustworkx.PyGraph()
         graph.add_nodes_from(self.nodes)
         graph.add_edges_from_no_data(self.edges)
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 3)
+        subgraphs = rustworkx.connected_subgraphs(graph, 3)
         self.assertEqual(subgraphs, self.expected_subgraphs[1]+self.expected_subgraphs[2]+self.expected_subgraphs[3])
 
     def test_full_graph(self):
         graph = rustworkx.PyGraph()
         graph.add_nodes_from(self.nodes)
         graph.add_edges_from_no_data(self.edges)
-        subgraphs = rustworkx.all_connected_subgraphs(graph, 8)
-        expected = set(copy(self.expected_subgraphs.values()))
-        for i, n in enumerate(self.nodes):
-            g = rustworkx.PyGraph()
-            g.add_node(i)
-            expected.add(g)
-        expected.add(graph)
-        self.assertEqual(subgraphs, expected)
+        subgraphs = rustworkx.connected_subgraphs(graph, 8)
+        self.assertEqual(subgraphs, list(self.expected_subgraphs.values()))
