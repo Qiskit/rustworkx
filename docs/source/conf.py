e94b1e4f81dd31790fa2883c3ee06f87666f01c2
Matthew Treinish <mtreinish@kortar.org>
2024-02-22 14:42:53 -0500

    Update version in docs config too

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2021, rustworkx Contributors'
 docs_url_prefix = ""
 
 # The short X.Y version.
-version = '0.14.0'
+version = '0.14'
 # The full version, including alpha/beta/rc tags.
-release = '0.14.0'
+release = '0.14.1'
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
