f2fd4e53ad6dfb5547c0f3a98459ffdb32588d9e
AlexanderIvrii <alexi@il.ibm.com>
2024-05-07 14:44:33 +0300

    another docs fix

--- src/coloring.rs
+++ src/coloring.rs
@@ -29,19 +29,19 @@ pub use rustworkx_core::coloring::GreedyStrategyCore;
 
 /// Greedy coloring strategies available for `graph_greedy_color`
 ///
-/// .. list-table::
+/// .. list-table:: Strategy description
 ///     :header-rows: 1
 ///
 ///     * - Strategy
 ///       - Reference
 ///     * - Degree
-///       - `Largest-first` strategy in :ref:`[1]` (section 1.2.2.2)
+///       - `Largest-first` strategy in [1] (section 1.2.2.2)
 ///     * - Saturation
-///       - `DSATUR` strategy in :ref:`[1]` (section 1.2.2.8)
+///       - `DSATUR` strategy in [1] (section 1.2.2.8)
 ///     * - IndependentSet
-///       - `GIS` strategy in :ref:`[1]` (section 1.2.2.9)
+///       - `GIS` strategy in [1] (section 1.2.2.9)
 ///
-/// .. _[1] Adrian Kosowski, and Krzysztof Manuszewski, Classical Coloring of Graphs, Graph Colorings, 2-19, 2004. ISBN 0-8218-3458-4.
+/// [1] Adrian Kosowski, and Krzysztof Manuszewski, Classical Coloring of Graphs, Graph Colorings, 2-19, 2004. ISBN 0-8218-3458-4.
 #[pyclass(module = "rustworkx")]
 #[derive(Clone, PartialEq)]
 pub enum GreedyStrategy {
