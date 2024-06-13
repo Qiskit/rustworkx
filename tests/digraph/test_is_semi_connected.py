953350166723a62aa3f16c8086f226c6b2303a6a
Catherine Lozano <clozano1@usc.edu>
2024-06-13 18:07:49 -0400

    added more large graph tests

--- tests/digraph/test_is_semi_connected.py
+++ tests/digraph/test_is_semi_connected.py
@@ -13,6 +13,7 @@
 import unittest
 
 import rustworkx
+import rustworkx.generators
 
 
 class TestSemiConnected(unittest.TestCase):
@@ -40,11 +41,27 @@ class TestSemiConnected(unittest.TestCase):
     def test_is_semi_connected_empty_graph(self):
         graph = rustworkx.PyDiGraph()
         with self.assertRaises(rustworkx.NullGraph):
-            rustworkx.is_weakly_connected(graph)
+            rustworkx.is_semi_connected(graph)
 
     def test_is_semi_connected_disconnected_graph(self):
         graph = rustworkx.PyDiGraph()
         graph.add_node(0)
         graph.add_node(1)
 
+        self.assertFalse(rustworkx.is_semi_connected(graph))
+
+    def test_is_semi_connected_directed_cycle_graph(self):
+        graph = rustworkx.generators.directed_cycle_graph(100)
+        self.assertTrue(rustworkx.is_semi_connected(graph))
+
+    def test_is_semi_connected_directed_path_graph(self):
+        graph = rustworkx.generators.directed_path_graph(100)
+        self.assertTrue(rustworkx.is_semi_connected(graph))
+  
+    def test_is_semi_connected_directed_star_graph(self):
+        graph = rustworkx.generators.directed_star_graph(100)
+        self.assertFalse(rustworkx.is_semi_connected(graph))
+  
+    def test_is_semi_connected_directed_grid_graph(self):
+        graph = rustworkx.generators.directed_grid_graph(50,50)
         self.assertFalse(rustworkx.is_semi_connected(graph))
