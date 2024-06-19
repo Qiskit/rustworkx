dc37bf9479195b467af098ecf321e4f0cb2bfa59
Ivan Carvalho <ivancarv@student.ubc.ca>
2024-06-18 23:40:24 -0400

    Add ground truth semi-connected implementation with Floyd-Warshall

--- tests/digraph/test_is_semi_connected.py
+++ tests/digraph/test_is_semi_connected.py
@@ -10,10 +10,19 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import itertools
 import unittest
 
 import rustworkx
 import rustworkx.generators
+
+
+def naive_semi_connected(graph):
+    dists = rustworkx.floyd_warshall(graph)
+    for u, v in itertools.combinations(graph.node_indices(), 2):
+        if (u not in dists[v]) and (v not in dists[u]):
+            return False
+    return True
 
 
 class TestSemiConnected(unittest.TestCase):
@@ -23,7 +32,7 @@ class TestSemiConnected(unittest.TestCase):
         graph.add_edge(0, 1, None)
         graph.add_edge(1, 2, None)
 
-        self.assertTrue(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_false(self):
         graph = rustworkx.PyDiGraph()
@@ -31,12 +40,12 @@ class TestSemiConnected(unittest.TestCase):
         graph.add_edge(0, 1, None)
         graph.add_edge(2, 1, None)
 
-        self.assertFalse(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_single_node(self):
         graph = rustworkx.PyDiGraph()
         graph.add_node(0)
-        self.assertTrue(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_empty_graph(self):
         graph = rustworkx.PyDiGraph()
@@ -48,20 +57,20 @@ class TestSemiConnected(unittest.TestCase):
         graph.add_node(0)
         graph.add_node(1)
 
-        self.assertTrue(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_directed_cycle_graph(self):
         graph = rustworkx.generators.directed_cycle_graph(100)
-        self.assertTrue(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_directed_path_graph(self):
         graph = rustworkx.generators.directed_path_graph(100)
-        self.assertTrue(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_directed_star_graph(self):
         graph = rustworkx.generators.directed_star_graph(100)
-        self.assertFalse(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
 
     def test_is_semi_connected_directed_grid_graph(self):
         graph = rustworkx.generators.directed_grid_graph(50, 50)
-        self.assertFalse(rustworkx.is_semi_connected(graph))
+        self.assertEqual(rustworkx.is_semi_connected(graph), naive_semi_connected(graph))
