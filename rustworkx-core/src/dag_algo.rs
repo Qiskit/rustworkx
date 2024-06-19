bced1c8079088b6efec9522c603b0e27a6985880
Your Name <arbel@il.ibm.com>
2024-06-19 12:27:20 +0300

    Rust newbie misses... more formatting

--- rustworkx-core/src/dag_algo.rs
+++ rustworkx-core/src/dag_algo.rs
@@ -499,7 +499,7 @@ where
 /// ```rust
 /// use petgraph::graph::DiGraph;
 /// use rustworkx_core::dag_algo::collect_runs;
-/// 
+///
 /// let mut graph: DiGraph<i32, ()> = DiGraph::new();
 /// let n1 = graph.add_node(-1);
 /// let n2 = graph.add_node(2);
