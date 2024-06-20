c8f32e3af39df12a938aa5130339662e5b8f043a
Ivan Carvalho <ivancarv@student.ubc.ca>
2024-06-19 20:36:03 -0400

    Remove SciPy as a development dependency

--- tests/digraph/test_pagerank.py
+++ tests/digraph/test_pagerank.py
@@ -9,7 +9,43 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+#
+# NetworkX is distributed with the 3-clause BSD license.
+#
+#   Copyright (C) 2004-2020, NetworkX Developers
+#   Aric Hagberg <hagberg@lanl.gov>
+#   Dan Schult <dschult@colgate.edu>
+#   Pieter Swart <swart@lanl.gov>
+#   All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are
+#   met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * Neither the name of the NetworkX Developers nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 # These tests are adapated from the networkx test cases:
 # https://github.com/networkx/networkx/blob/cea310f9066efc0d5ff76f63d33dbc3eefe61f6b/networkx/algorithms/link_analysis/tests/test_pagerank.py
 
@@ -19,16 +55,67 @@ import rustworkx
 import networkx as nx
 
 
+def pagerank_python(
+    G,
+    alpha=0.85,
+    personalization=None,
+    max_iter=100,
+    tol=1.0e-6,
+    nstart=None,
+    weight="weight",
+    dangling=None,
+):
+    if len(G) == 0:
+        return {}
+
+    D = G.to_directed()
+
+    # Create a copy in (right) stochastic form
+    W = nx.stochastic_graph(D, weight=weight)
+    N = W.number_of_nodes()
+
+    # Choose fixed starting vector if not given
+    if nstart is None:
+        x = dict.fromkeys(W, 1.0 / N)
+    else:
+        # Normalized nstart vector
+        s = sum(nstart.values())
+        x = {k: v / s for k, v in nstart.items()}
+
+    if personalization is None:
+        # Assign uniform personalization vector if not given
+        p = dict.fromkeys(W, 1.0 / N)
+    else:
+        s = sum(personalization.values())
+        p = {k: v / s for k, v in personalization.items()}
+
+    if dangling is None:
+        # Use personalization vector if dangling vector not specified
+        dangling_weights = p
+    else:
+        s = sum(dangling.values())
+        dangling_weights = {k: v / s for k, v in dangling.items()}
+    dangling_nodes = [n for n in W if W.out_degree(n, weight=weight) == 0.0]
+
+    # power iteration: make up to max_iter iterations
+    for _ in range(max_iter):
+        xlast = x
+        x = dict.fromkeys(xlast.keys(), 0)
+        danglesum = alpha * sum(xlast[n] for n in dangling_nodes)
+        for n in x:
+            # this matrix multiply looks odd because it is
+            # doing a left multiply x^T=xlast^T*W
+            for _, nbr, wt in W.edges(n, data=weight):
+                x[nbr] += alpha * xlast[n] * wt
+            x[n] += danglesum * dangling_weights.get(n, 0) + (1.0 - alpha) * p.get(n, 0)
+        # check convergence, l1 norm
+        err = sum(abs(x[n] - xlast[n]) for n in x)
+        if err < N * tol:
+            return x
+    raise nx.PowerIterationFailedConvergence(max_iter)
+
+
 class TestPageRank(unittest.TestCase):
-    def setUp(self) -> None:
-        try:
-            # required for networkx.pagerank to work
-            import scipy
-
-            self.assertIsNotNone(scipy.__version__)
-        except ModuleNotFoundError:
-            self.skipTest("SciPy is not installed, skipping PageRank tests")
-
     def test_with_dangling_node(self):
         edges = [
             (0, 1),
@@ -53,7 +140,7 @@ class TestPageRank(unittest.TestCase):
         tol = 1.0e-8
 
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha, tol=tol)
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha, tol=tol)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha, tol=tol)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
@@ -84,7 +171,7 @@ class TestPageRank(unittest.TestCase):
         tol = 1.0e-8
 
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha, tol=tol, dangling=dangling)
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha, tol=tol, dangling=dangling)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha, tol=tol, dangling=dangling)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
@@ -141,7 +228,7 @@ class TestPageRank(unittest.TestCase):
         alpha = 0.85
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha, nstart=nstart)
         nx_graph = nx.DiGraph(list(rx_graph.edge_list()))
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha, nstart=nstart)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha, nstart=nstart)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
@@ -152,7 +239,7 @@ class TestPageRank(unittest.TestCase):
         alpha = 0.85
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha, personalization=personalize)
         nx_graph = nx.DiGraph(list(rx_graph.edge_list()))
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha, personalization=personalize)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha, personalization=personalize)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
@@ -163,7 +250,7 @@ class TestPageRank(unittest.TestCase):
         alpha = 0.85
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha, personalization=personalize)
         nx_graph = nx.DiGraph(list(rx_graph.edge_list()))
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha, personalization=personalize)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha, personalization=personalize)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
@@ -192,7 +279,7 @@ class TestPageRank(unittest.TestCase):
 
         alpha = 0.9
         rx_ranks = rustworkx.pagerank(rx_graph, alpha=alpha)
-        nx_ranks = nx.pagerank(nx_graph, alpha=alpha)
+        nx_ranks = pagerank_python(nx_graph, alpha=alpha)
 
         for v in rx_graph.node_indices():
             self.assertAlmostEqual(rx_ranks[v], nx_ranks[v], delta=1.0e-4)
