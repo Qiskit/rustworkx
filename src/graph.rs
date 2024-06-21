7325552ed1f823b32ce41023fe959b82c70b9267
Matthew Treinish <mtreinish@kortar.org>
2024-06-21 17:20:40 -0400

    Apply suggestions from code review

--- src/graph.rs
+++ src/graph.rs
@@ -143,7 +143,7 @@ use petgraph::visit::{
 ///     only a potential performance optimization if the complete size of the graph is known in
 ///     advance (default 0: no overallocation).
 /// :param int initial_edge_count: The graph will be allocated with enough capacity to store this
-///     many edges before needing to grow.  This does not prepopulate any nodes with data, it is
+///     many edges before needing to grow.  This does not prepopulate any edges with data, it is
 ///     only a potential performance optimization if the complete size of the graph is known in
 ///     advance (default 0: no overallocation).
 #[pyclass(mapping, module = "rustworkx", subclass)]
