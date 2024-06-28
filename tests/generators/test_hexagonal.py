fix factor of two in coordinates calculation

tests/generators/test_hexagonal.py
@@ -584,20 +584,27 @@ class TestHexagonalLatticeGraph(unittest.TestCase):
     def test_hexagonal_graph_periodic_odd_columns(self):
         with self.assertRaises(ValueError):
             rustworkx.generators.hexagonal_lattice_graph(4, 5, periodic=True)
-    
+
     def test_hexagonal_graph_with_positions(self):
         graph = rustworkx.generators.hexagonal_lattice_graph(2, 2, with_positions=True)
         positions = graph.nodes()
-        hexagons = [[0, 1, 2, 7, 6, 5], [2, 3, 4, 7, 8, 9]]
+        hexagons = [
+            [0, 1, 2, 7, 6, 5],
+            [2, 3, 4, 9, 8, 7],
+            [6, 7, 8, 13, 12, 11],
+            [8, 9, 10, 15, 14, 13],
+        ]
         C6 = rustworkx.generators.cycle_graph(6)
         for h in hexagons:
             self.assertTrue(rustworkx.is_isomorphic(graph.subgraph(h), C6))
             coordinates = np.array([positions[node] for node in h])
-            vectors = [ coordinates[(ii+1)%6] - coordinates[ii] for ii in range(6) ]
+            vectors = [coordinates[(ii + 1) % 6] - coordinates[ii] for ii in range(6)]
 
             for v in vectors:
-                # Check that each side of the hexagon has length 2
-                self.assertAlmostEqual(np.linalg.norm(v), 2.0, 12)
+                # Check that each side of the hexagon has length 1
+                self.assertAlmostEqual(np.linalg.norm(v), 1.0, 12)
             for ii in range(6):
                 # Check that the angle between each consecutive pair of sides is pi/3
-                self.assertAlmostEqual(np.dot(vectors[ii], vectors[(ii+1)%6]), 2 * 2 * np.cos(np.pi/3), 12)
+                self.assertAlmostEqual(
+                    np.dot(vectors[ii], vectors[(ii + 1) % 6]), np.cos(np.pi / 3), 12
+                )