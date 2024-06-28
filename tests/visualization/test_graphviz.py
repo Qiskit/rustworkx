Qiskit/rustworkx

commit 70b71260ecc1d7aeb9bc5a456e87b4c89b8f5370

Potential fix to the tooltip with special characters  bug (#1203)
* Potential solution

* Push update

* Fixed linting issues

* Fix to linting issues

* minor fix

* using serde_json instead of manually escaping

* fix import, and added test case

* Updated test logic

* updated test to check to_dot

* fix clippy issues

* created release notes

* fix release notes

* fix linting issue in test

* remove unused import

* fixing quotes issue

* fixing "\" issue and updating tests

* updating more test to have quotes

* Update dot_utils.rs

* Apply suggestions from code review

* Collect vector of results in Rust

* Fix error handling

* Update releasenotes/notes/fix-graphviz-draw-tooltip-3f697d71c4b79e60.yaml

* Update releasenotes/notes/fix-graphviz-draw-tooltip-3f697d71c4b79e60.yaml

* Update releasenotes/notes/fix-graphviz-draw-tooltip-3f697d71c4b79e60.yaml

---------

Co-authored-by: Ivan Carvalho <8753214+IvanIsCoding@users.noreply.github.com>

src/dot_utils.rs
@@ -82,17 +82,18 @@ fn attr_map_to_string<'a>(
     if attrs.is_empty() {
         return Ok("".to_string());
     }
-
     let attr_string = attrs
         .iter()
         .map(|(key, value)| {
-            if key == "label" {
-                format!("{}=\"{}\"", key, value)
-            } else {
-                format!("{}={}", key, value)
-            }
+            let escaped_value = serde_json::to_string(value).map_err(|_err| {
+                pyo3::exceptions::PyValueError::new_err("could not escape character")
+            })?;
+            let escaped_value = &escaped_value.get(1..escaped_value.len() - 1).ok_or(
+                pyo3::exceptions::PyValueError::new_err("could not escape character"),
+            )?;
+            Ok(format!("{}=\"{}\"", key, escaped_value))
         })
-        .collect::<Vec<String>>()
+        .collect::<PyResult<Vec<String>>>()?
         .join(", ");
     Ok(format!("[{}]", attr_string))
 }

tests/digraph/test_dot.py
@@ -43,9 +43,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'digraph {\n0 [color=black, fillcolor=green, label="a", '
-            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
-            'style=filled];\n0 -> 1 [label="1", name=1];\n}\n'
+            'digraph {\n0 [color="black", fillcolor="green", label="a", '
+            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
+            'style="filled"];\n0 -> 1 [label="1", name="1"];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge, filename=self.path)
         self.addCleanup(os.remove, self.path)

tests/graph/test_dot.py
@@ -43,9 +43,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'graph {\n0 [color=black, fillcolor=green, label="a", style=filled'
-            '];\n1 [color=black, fillcolor=red, label="a", style=filled];'
-            '\n0 -- 1 [label="1", name=1];\n}\n'
+            'graph {\n0 [color="black", fillcolor="green", label="a", style="filled"'
+            '];\n1 [color="black", fillcolor="red", label="a", style="filled"];'
+            '\n0 -- 1 [label="1", name="1"];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge)
         self.assertEqual(expected, res)
@@ -70,9 +70,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'digraph {\n0 [color=black, fillcolor=green, label="a", '
-            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
-            'style=filled];\n0 -> 1 [label="1", name=1];\n}\n'
+            'digraph {\n0 [color="black", fillcolor="green", label="a", '
+            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
+            'style="filled"];\n0 -> 1 [label="1", name="1"];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge)
         self.assertEqual(expected, res)
@@ -97,9 +97,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'graph {\n0 [color=black, fillcolor=green, label="a", '
-            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
-            'style=filled];\n0 -- 1 [label="1", name=1];\n}\n'
+            'graph {\n0 [color="black", fillcolor="green", label="a", '
+            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
+            'style="filled"];\n0 -- 1 [label="1", name="1"];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge, filename=self.path)
         self.addCleanup(os.remove, self.path)

tests/visualization/test_graphviz.py
@@ -14,7 +14,6 @@ import os
 import subprocess
 import tempfile
 import unittest
-
 import rustworkx
 from rustworkx.visualization import graphviz_draw
 
@@ -150,3 +149,36 @@ class TestGraphvizDraw(unittest.TestCase):
         self.assertTrue(os.path.isfile("test_graphviz_filename.svg"))
         if not SAVE_IMAGES:
             self.addCleanup(os.remove, "test_graphviz_filename.svg")
+
+    def test_escape_sequences(self):
+        # Create a simple graph
+        graph = rustworkx.generators.path_graph(2)
+
+        escape_sequences = {
+            "\\n": "\n",  # Newline
+            "\\t": "\t",  # Horizontal tab
+            "\\'": "'",  # Single quote
+            '\\"': '"',  # Double quote
+            "\\\\": "\\",  # Backslash
+            "\\r": "\r",  # Carriage return
+            "\\b": "\b",  # Backspace
+            "\\f": "\f",  # Form feed
+        }
+
+        for escaped_seq, raw_seq in escape_sequences.items():
+
+            def node_attr(node):
+                """
+                Define node attributes including escape sequences for labels and tooltips.
+                """
+                label = f"label{escaped_seq}"
+                tooltip = f"tooltip{escaped_seq}"
+                return {"label": label, "tooltip": tooltip}
+
+            # Draw the graph using graphviz_draw
+            dot_str = graph.to_dot(node_attr)
+
+            # Assert that the escape sequences are correctly placed and escaped in the dot string
+            self.assertIn(
+                escaped_seq, dot_str, f"Escape sequence {escaped_seq} not found in dot output"
+            )

releasenotes/notes/fix-graphviz-draw-tooltip-3f697d71c4b79e60.yaml
@@ -1,0 +1,14 @@
+---
+fixes:
+  - |
+      :func:`.graphviz_draw` can now handle special characters
+
+          .. jupyter-execute::
+
+                import rustworkx as rx
+                from rustworkx.visualization import graphviz_draw
+
+                graphviz_draw(
+                    rx.generators.path_graph(2),
+                    node_attr_fn=lambda x: {"label": "the\nlabel", "tooltip": "the\ntooltip"},
+                )

commit 2292b1f95afc4bb56ed2ade8ea0876a22a784e84

Fix dot file generation
This commit fixes a regression introduced in #1203. That PR was
attempting to fix missing character escaping in some string fields
but it was doing so too eagerly. This would result in invalid dot
files being produced for users upgrading from rustworkx < 0.15.0 that
were wrapping strings in quotes as needed previously. For example if you
were setting `'color="#aaaaaa"'` previously before this would become
`color="\"#aaaaaa\""` after #1203. In order to quickly release a 0.15.1
this commit reverts the dot generation component of #1203 but updates
the code to wrap tooltip in addition to label which was what the
original bug reported. In 0.16.0 we should investigate adding a flag to
control the escaping behavior of the function to either decide to wrap
values in quotes or not.

releasenotes/notes/fix-graphviz-escaping-39a2d1cebb586eca.yaml
@@ -1,0 +1,21 @@
+---
+fixes:
+  - |
+    Fixed an issue in the :func:`~.graphviz_draw`, :meth:`.PyGraph.to_dot`, and
+    :meth:`.PyDiGraph.to_dot` which was incorrectly escaping strings when
+    upgrading to 0.15.0. In earlier versions of rustworkx if you manually
+    placed quotes in a string for an attr callback get that to pass through to 
+    the output dot file this was incorrectly being converted in rustworkx 0.15.0
+    to duplicate the quotes and escape them. For example, if you defined a
+    callback like::
+
+        def color_node(_node):
+            return {
+                "color": '"#422952"'
+            }
+    to set the color attribute in the output dot file with the string
+    `"#422952"` (with the quotes) this was incorrectly being converted to
+    `\"#422952\"`. This no longer occurs, in rustworkx 0.16.0 there will likely
+    be additional options exposed in :func:`~.graphviz_draw`,
+    :meth:`.PyGraph.to_dot`, and :meth:`.PyDiGraph.to_dot` to expose further
+    options around this.

src/dot_utils.rs
@@ -64,6 +64,8 @@ where
     Ok(())
 }
 
+static ATTRS_TO_ESCAPE: [&str; 2] = ["label", "tooltip"];
+
 /// Convert an attr map to an output string
 fn attr_map_to_string<'a>(
     py: Python,
@@ -85,15 +87,13 @@ fn attr_map_to_string<'a>(
     let attr_string = attrs
         .iter()
         .map(|(key, value)| {
-            let escaped_value = serde_json::to_string(value).map_err(|_err| {
-                pyo3::exceptions::PyValueError::new_err("could not escape character")
-            })?;
-            let escaped_value = &escaped_value.get(1..escaped_value.len() - 1).ok_or(
-                pyo3::exceptions::PyValueError::new_err("could not escape character"),
-            )?;
-            Ok(format!("{}=\"{}\"", key, escaped_value))
+            if ATTRS_TO_ESCAPE.contains(&key.as_str()) {
+                format!("{}=\"{}\"", key, value)
+            } else {
+                format!("{}={}", key, value)
+            }
         })
-        .collect::<PyResult<Vec<String>>>()?
+        .collect::<Vec<String>>()
         .join(", ");
     Ok(format!("[{}]", attr_string))
 }

tests/digraph/test_dot.py
@@ -43,9 +43,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'digraph {\n0 [color="black", fillcolor="green", label="a", '
-            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
-            'style="filled"];\n0 -> 1 [label="1", name="1"];\n}\n'
+            'digraph {\n0 [color=black, fillcolor=green, label="a", '
+            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
+            'style=filled];\n0 -> 1 [label="1", name=1];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge, filename=self.path)
         self.addCleanup(os.remove, self.path)

tests/graph/test_dot.py
@@ -43,9 +43,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'graph {\n0 [color="black", fillcolor="green", label="a", style="filled"'
-            '];\n1 [color="black", fillcolor="red", label="a", style="filled"];'
-            '\n0 -- 1 [label="1", name="1"];\n}\n'
+            'graph {\n0 [color=black, fillcolor=green, label="a", style=filled'
+            '];\n1 [color=black, fillcolor=red, label="a", style=filled];'
+            '\n0 -- 1 [label="1", name=1];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge)
         self.assertEqual(expected, res)
@@ -70,9 +70,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'digraph {\n0 [color="black", fillcolor="green", label="a", '
-            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
-            'style="filled"];\n0 -> 1 [label="1", name="1"];\n}\n'
+            'digraph {\n0 [color=black, fillcolor=green, label="a", '
+            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
+            'style=filled];\n0 -> 1 [label="1", name=1];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge)
         self.assertEqual(expected, res)
@@ -97,9 +97,9 @@ class TestDot(unittest.TestCase):
         )
         graph.add_edge(0, 1, dict(label="1", name="1"))
         expected = (
-            'graph {\n0 [color="black", fillcolor="green", label="a", '
-            'style="filled"];\n1 [color="black", fillcolor="red", label="a", '
-            'style="filled"];\n0 -- 1 [label="1", name="1"];\n}\n'
+            'graph {\n0 [color=black, fillcolor=green, label="a", '
+            'style=filled];\n1 [color=black, fillcolor=red, label="a", '
+            'style=filled];\n0 -- 1 [label="1", name=1];\n}\n'
         )
         res = graph.to_dot(lambda node: node, lambda edge: edge, filename=self.path)
         self.addCleanup(os.remove, self.path)

tests/visualization/test_graphviz.py
@@ -150,6 +150,55 @@ class TestGraphvizDraw(unittest.TestCase):
         if not SAVE_IMAGES:
             self.addCleanup(os.remove, "test_graphviz_filename.svg")
 
+    def test_qiskit_style_visualization(self):
+        """This test is to test visualizations like qiskit performs which regressed in 0.15.0."""
+        graph = rustworkx.generators.cycle_graph(4)
+        colors = ["#422952", "#492d58", "#4f305c", "#5e3767"]
+        edge_colors = ["#4d2f5b", "#693d6f", "#995a88", "#382449"]
+        pos = [(0, 0), (0, 1), (1, 0), (1, 1)]
+        for node in graph.node_indices():
+            graph[node] = node
+
+        for edge in graph.edge_indices():
+            graph.update_edge_by_index(edge, edge)
+
+        def color_node(node):
+            out_dict = {
+                "label": str(node),
+                "color": f'"{colors[node]}"',
+                "pos": f'"{pos[node][0]}, {pos[node][1]}"',
+                "fontname": '"DejaVu Sans"',
+                "pin": "True",
+                "shape": "circle",
+                "style": "filled",
+                "fillcolor": f'"{colors[node]}"',
+                "fontcolor": "white",
+                "fontsize": "10",
+                "height": "0.322",
+                "fixedsize": "True",
+            }
+            return out_dict
+
+        def color_edge(edge):
+            out_dict = {
+                "color": f'"{edge_colors[edge]}"',
+                "fillcolor": f'"{edge_colors[edge]}"',
+                "penwidth": str(5),
+            }
+            return out_dict
+
+        graphviz_draw(
+            graph,
+            node_attr_fn=color_node,
+            edge_attr_fn=color_edge,
+            filename="test_qiskit_style_visualization.png",
+            image_type="png",
+            method="neato",
+        )
+        self.assertTrue(os.path.isfile("test_qiskit_style_visualization.png"))
+        if not SAVE_IMAGES:
+            self.addCleanup(os.remove, "test_qiskit_style_visualization.png")
+
     def test_escape_sequences(self):
         # Create a simple graph
         graph = rustworkx.generators.path_graph(2)