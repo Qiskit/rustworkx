bd1ee6cb44ba63c841a3e08456d72e622cfa3bf9
jpacold <jpacold@gmail.com>
2024-07-04 16:15:42 -0600

    implement `_total_edge_weight`

--- rustworkx-core/src/community/metrics.rs
+++ rustworkx-core/src/community/metrics.rs
@@ -1,17 +1,26 @@
-use num_traits::Float;
+use num_traits::Num;
 use std::collections::HashSet;
 use std::vec::Vec;
 
 use petgraph::graph::NodeIndex;
-use petgraph::visit::{Data, NodeIndexable};
+use petgraph::visit::{Data, EdgeRef, IntoEdgeReferences, NodeIndexable};
 
 fn _number_internal_edges(graph: &G, community: &HashSet<NodeIndex>) -> u64 {}
 
-fn _total_degree(graph: &G, community: &HashSet<NodeIndex>) -> u64 {}
+fn _total_edge_weight<G, W>(graph: &G) -> W
+where
+    G: Data<EdgeWeight = W> + IntoEdgeReferences,
+    W: Num + Copy,
+{
+    graph
+        .edge_references()
+        .map(|edge| *edge.weight())
+        .fold(W::zero(), |s, e| s + e)
+}
 
 pub fn modularity<G, W>(graph: &G, communities: &Vec<Vec<NodeIndex>>, resolution: f64) -> f64
 where
-    G: Data<EdgeWeight = W> + NodeIndexable,
-    W: Float,
+    G: Data<EdgeWeight = W> + NodeIndexable + IntoEdgeReferences,
+    W: Num,
 {
 }
