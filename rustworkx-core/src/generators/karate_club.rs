d0ae8cb6d75cec8065ac3dd332be653f7fc44f68
Ivan Carvalho <ivancarv@student.ubc.ca>
2024-09-14 14:15:03 -0400

    Add simple Rust docstring

--- rustworkx-core/src/generators/karate_club.rs
+++ rustworkx-core/src/generators/karate_club.rs
@@ -57,6 +57,10 @@ const ZACHARY: &str = "\
 
 const MR_HI_MEMBERS: [usize; 17] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 16, 17, 19, 21];
 
+/// Generates Zachary's Karate Club graph.
+///
+/// Zachary's Karate Club graph is a well-known social network that represents
+/// the relations between 34 members of a karate club.
 pub fn karate_club_graph<G, T, F, H, M>(
     mut default_node_weight: F,
     mut default_edge_weight: H,
