9d9e5efb347fe2fe79a1257280568921552af5f8
Ivan Carvalho <ivancarv@student.ubc.ca>
2025-01-14 20:47:03 -0500

    Black

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -16,6 +16,7 @@ import unittest
 
 from typing import Optional
 
+
 class TestAnnotationSubscriptions(unittest.TestCase):
     def test_digraph(self):
         graph: rx.PyDiGraph[int, int] = rx.PyDiGraph()
