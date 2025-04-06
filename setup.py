d5696768aa2ece1931dd59e67b67abbcb70af282
Ivan Carvalho <ivancarv@student.ubc.ca>
2025-04-05 20:38:34 -0400

    Move even from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -5,22 +5,8 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-
-import os
-
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
-
-# If RUST_DEBUG is set, force compiling in debug mode. Else, use the default behavior of whether
-# it's an editable installation.
-rustworkx_debug = True if os.getenv("RUSTWORKX_DEBUG") == "1" else None
-
-RUST_EXTENSIONS = [RustExtension("rustworkx.rustworkx", "Cargo.toml",
-                                 binding=Binding.PyO3, debug=rustworkx_debug)]
-RUST_OPTS ={"bdist_wheel": {"py_limited_api": "cp39"}}
 
 setup(
-    rust_extensions=RUST_EXTENSIONS,
-    zip_safe=False,
-    options=RUST_OPTS,
+    options={"bdist_wheel": {"py_limited_api": "cp39"}},
 )
