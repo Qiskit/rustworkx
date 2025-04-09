f8d7ba020d5a99cb8df9b53d50719a5451305d52
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2025-04-09 11:44:55 -0400

    Lint: Fix indentation

--- rustworkx-core/src/connectivity/cycle_basis.rs
+++ rustworkx-core/src/connectivity/cycle_basis.rs
@@ -35,10 +35,9 @@ use std::hash::Hash;
 ///
 /// * `graph` - The graph in which to find the basis.
 /// * `root` - Optional node index for starting the basis search. If not
-///     specified, an arbitrary node is chosen.
+///   specified, an arbitrary node is chosen.
 /// * `edges` - bool for when the user requests the edges instead
-///     of the nodes of the cycles.
-
+///   of the nodes of the cycles.
 fn inner_cycle_basis<G>(
     graph: G,
     root: Option<G::NodeId>,
