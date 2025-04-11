455cc3a9833ad450cd47580192f19f8fb8b02053
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2025-04-11 19:48:28 +0530

    Adding fuzz targets for special graphs
    Added Fuzz targets for heavy-hex graph and lollipop graph to test their robustness for various inputs.

---
+++ rustworkx-core/fuzz/fuzz_targets/test_fuzz_heavy_hex_graph.rs
@@ -0,0 +1,28 @@
+#![no_main]
+
+use arbitrary::{Arbitrary, Unstructured};
+use libfuzzer_sys::fuzz_target;
+use rustworkx_core::generators::heavy_hex_graph;
+use rustworkx_core::petgraph::graph::UnGraph;
+
+#[derive(Debug, Arbitrary)]
+struct HeavyHexInput {
+    d: usize,
+    bidirectional: bool,
+}
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(input) = HeavyHexInput::arbitrary(&mut Unstructured::new(data)) {
+        fuzz_heavy_hex_graph(input);
+    }
+});
+
+fn fuzz_heavy_hex_graph(input: HeavyHexInput) {
+    let d = input.d % 21 | 1;
+    let _ = heavy_hex_graph::<UnGraph<(), ()>, (), _, _, ()>(
+        d,
+        || (),
+        || (),
+        input.bidirectional,
+    );
+}
