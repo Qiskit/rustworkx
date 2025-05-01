355615b28a78051df9f6c8000f0d1f9ad73f6ad1
Matthew Treinish <mtreinish@kortar.org>
2025-05-01 13:12:23 -0400

    Add distance_matrix to rustworkx-core
    This commit moves the distance matrix functionality to rustworkx. This
    is mostly a straightforward migration as the functionality was written
    in a generic way already. The only difference is how node holes were
    handled, the this opted to split the functions into 2, a verison that
    assumes the graph has compact indices and one that doesn't.

--- rustworkx-core/src/shortest_path/mod.rs
+++ rustworkx-core/src/shortest_path/mod.rs
@@ -19,10 +19,13 @@ mod all_shortest_paths;
 mod astar;
 mod bellman_ford;
 mod dijkstra;
+mod distance_matrix;
 mod k_shortest_path;
 
 pub use all_shortest_paths::all_shortest_paths;
 pub use astar::astar;
 pub use bellman_ford::{bellman_ford, negative_cycle_finder};
 pub use dijkstra::dijkstra;
+pub use distance_matrix::distance_matrix;
+pub use distance_matrix::distance_matrix_compacted;
 pub use k_shortest_path::k_shortest_path;
