a44bcfc0c0ae36ea188218d8188e17fbec41a267
Ivan Carvalho <ivancarvalho@gatech.edu>
2025-05-14 08:45:24 -0400

    Trying to make local test happen

--- tests/pyodide_smoke_test/smoke_test.mjs
+++ tests/pyodide_smoke_test/smoke_test.mjs
@@ -1,13 +1,28 @@
 import { loadPyodide } from "pyodide";
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function rustworkx_python() {
   let pyodide = await loadPyodide();
   await pyodide.loadPackage("micropip");
   const micropip = pyodide.pyimport("micropip");
-  await micropip.install(`http://localhost:8000/rustworkx-0.17.0-cp39-abi3-pyodide_2024_0_wasm32.whl`);
+
+  // Load rustworkx wheel into memory
+  const filename = 'rustworkx-0.17.0-cp39-abi3-pyodide_2024_0_wasm32.whl';
+
+  const wheelPath = path.resolve(filename);
+  const wheelData = fs.readFileSync(wheelPath);
+  // console.log("Read data");
+
+  pyodide.FS.mkdir('/tmp', 0o777);
+  pyodide.FS.writeFile(`/tmp/${filename}`, new Uint8Array(wheelData));
+
+  // console.log(`Successfully loaded ${filename} into Pyodide's virtual file system`);
+
+  await micropip.install(`emfs://tmp/${filename}`);
   return pyodide.runPythonAsync(`
-	  import rustworkx
-	  print(rustworkx.__version__)
+    import rustworkx
+    print(rustworkx.__version__)
   `);
 }
 
