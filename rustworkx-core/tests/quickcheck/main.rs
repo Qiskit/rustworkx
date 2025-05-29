12984adb01581ff688d67c5e467f52b9ac0cc902
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2025-05-30 00:27:49 +0530

    Update main.rs

--- rustworkx-core/tests/quickcheck/main.rs
+++ rustworkx-core/tests/quickcheck/main.rs
@@ -1,10 +1,1 @@
-macro_rules! common_test {
-    ($func:ident, $graph_ty:ty) => {
-        #[test]
-        fn $func() {
-            rustworkx_tests::lollipop::$func::<$graph_ty>();
-        }
-    };
-}
-
 mod special_graph;
