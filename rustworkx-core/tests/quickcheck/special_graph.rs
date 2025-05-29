7840a2c606dcb3065e960bbd28df7437e59312c7
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2025-05-30 02:21:06 +0530

    Added property checks for lollipop graph (#1453)
    * Added property checks for lollipop graph
    
    Added a test using QuickCheck to verify that the lollipop_graph generator produces graphs with the correct number of nodes and edges based on the input sizes of the mesh and path.
    
    Updated the Cargo.toml to add dependecies and test target.
    
    * Update based on comment
    
    * Updates based on comments
    
    * Lint issue fixed
    
    * Update main.rs
    
    ---------
    
    Co-authored-by: Ivan Carvalho <8753214+IvanIsCoding@users.noreply.github.com>

---
+++ rustworkx-core/tests/quickcheck/special_graph.rs
@@ -0,0 +1,44 @@
+use petgraph::graph::UnGraph;
+use quickcheck::{quickcheck, TestResult};
+use rustworkx_core::generators::lollipop_graph;
+
+#[test]
+fn prop_lollipop_graph_structure() {
+    fn prop(mesh_size: usize, path_size: usize) -> TestResult {
+        // Constrain input space for sanity
+        let mesh_size = mesh_size % 64;
+        let path_size = path_size % 64;
+
+        if mesh_size + path_size == 0 {
+            return TestResult::discard();
+        }
+        if mesh_size <= 1 {
+            return TestResult::discard();
+        }
+
+        let graph = match lollipop_graph::<UnGraph<(), ()>, (), _, _, ()>(
+            Some(mesh_size),
+            Some(path_size),
+            None,
+            None,
+            || (),
+            || (),
+        ) {
+            Ok(g) => g,
+            Err(_) => return TestResult::error("Unexpected error in graph generation"),
+        };
+
+        let expected_nodes = mesh_size + path_size;
+        let mesh_edges = mesh_size * (mesh_size - 1) / 2;
+        let path_edges = path_size.saturating_sub(1);
+        let connector = if path_size > 0 { 1 } else { 0 };
+        let expected_edges = mesh_edges + path_edges + connector;
+
+        let node_match = graph.node_count() == expected_nodes;
+        let edge_match = graph.edge_count() == expected_edges;
+
+        TestResult::from_bool(node_match && edge_match)
+    }
+
+    quickcheck(prop as fn(usize, usize) -> TestResult);
+}
