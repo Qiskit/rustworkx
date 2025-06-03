ffd568bd14883d5b467acc76bb9d5fcf45daa60d
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-06-03 16:50:02 -0700

    adding initial subgraph_with_nodemap implementation

--- src/graph.rs
+++ src/graph.rs
@@ -1907,7 +1907,60 @@ impl PyGraph {
         Ok(res.index())
     }
 
-    /// Return a new PyGraph object for a subgraph of this graph
+    /// Return a new PyGraph object for a subgraph of this graph and a NodeMap object that maps the nodes of the subgraph to the nodes of the original graph.
+    ///
+    /// :param list[int] nodes: A list of node indices to generate the subgraph
+    ///     from. If a node index is included that is not present in the graph
+    ///     it will silently be ignored.
+    /// :param bool preserve_attrs: If set to the True the attributes of the PyGraph
+    ///     will be copied by reference to be the attributes of the output
+    ///     subgraph. By default this is set to False and the :attr:`~.PyGraph.attrs`
+    ///     attribute will be ``None`` in the subgraph.
+    ///
+    /// :returns: A tuple containing a new PyGraph object representing a subgraph of this graph
+    ///     and a NodeMap object that maps the nodes of the subgraph to the nodes of the original graph.
+    ///     It is worth noting that node and edge weight/data payloads are
+    ///     passed by reference so if you update (not replace) an object used
+    ///     as the weight in graph or the subgraph it will also be updated in
+    ///     the other.
+    /// :rtype: PyGraph
+    ///
+    #[pyo3(signature=(nodes, preserve_attrs=false), text_signature = "(self, nodes, /, preserve_attrs=False)")]
+    pub fn subgraph_with_nodemap(&self, py: Python, nodes: Vec<usize>, preserve_attrs: bool) -> (PyGraph, NodeMap) {
+        let node_set: HashSet<usize> = nodes.iter().cloned().collect();
+        // mapping from original node index to new node index
+        let mut node_map: HashMap<NodeIndex, NodeIndex> = HashMap::with_capacity(nodes.len());
+        // mapping from new node index to original node index
+        let mut node_dict: DictMap<usize, usize> = DictMap::with_capacity(nodes.len());
+        let node_filter = |node: NodeIndex| -> bool { node_set.contains(&node.index()) };
+        let mut out_graph = StablePyGraph::<Undirected>::default();
+        let filtered = NodeFiltered(&self.graph, node_filter);
+        for node in filtered.node_references() {
+            let new_node = out_graph.add_node(node.1.clone_ref(py));
+            node_map.insert(node.0, new_node);
+            node_dict.insert(new_node.index(), node.0.index());
+        }
+        for edge in filtered.edge_references() {
+            let new_source = *node_map.get(&edge.source()).unwrap();
+            let new_target = *node_map.get(&edge.target()).unwrap();
+            out_graph.add_edge(new_source, new_target, edge.weight().clone_ref(py));
+        }
+        let attrs = if preserve_attrs {
+            self.attrs.clone_ref(py)
+        } else {
+            py.None()
+        };
+        let node_map = NodeMap { node_map: node_dict };
+        let subgraph = PyGraph {
+            graph: out_graph,
+            node_removed: false,
+            multigraph: self.multigraph,
+            attrs,
+        };
+        (subgraph, node_map)
+    }
+
+    /// Return a new PyGraph object for a subgraph of this graph.
     ///
     /// :param list[int] nodes: A list of node indices to generate the subgraph
     ///     from. If a node index is included that is not present in the graph
@@ -1926,31 +1979,8 @@ impl PyGraph {
     ///
     #[pyo3(signature=(nodes, preserve_attrs=false), text_signature = "(self, nodes, /, preserve_attrs=False)")]
     pub fn subgraph(&self, py: Python, nodes: Vec<usize>, preserve_attrs: bool) -> PyGraph {
-        let node_set: HashSet<usize> = nodes.iter().cloned().collect();
-        let mut node_map: HashMap<NodeIndex, NodeIndex> = HashMap::with_capacity(nodes.len());
-        let node_filter = |node: NodeIndex| -> bool { node_set.contains(&node.index()) };
-        let mut out_graph = StablePyGraph::<Undirected>::default();
-        let filtered = NodeFiltered(&self.graph, node_filter);
-        for node in filtered.node_references() {
-            let new_node = out_graph.add_node(node.1.clone_ref(py));
-            node_map.insert(node.0, new_node);
-        }
-        for edge in filtered.edge_references() {
-            let new_source = *node_map.get(&edge.source()).unwrap();
-            let new_target = *node_map.get(&edge.target()).unwrap();
-            out_graph.add_edge(new_source, new_target, edge.weight().clone_ref(py));
-        }
-        let attrs = if preserve_attrs {
-            self.attrs.clone_ref(py)
-        } else {
-            py.None()
-        };
-        PyGraph {
-            graph: out_graph,
-            node_removed: false,
-            multigraph: self.multigraph,
-            attrs,
-        }
+        let (subgraph, _) = self.subgraph_with_nodemap(py, nodes, preserve_attrs);
+        subgraph
     }
 
     /// Return a new PyGraph object for an edge induced subgraph of this graph
