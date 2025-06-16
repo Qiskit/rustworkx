afe7e9bb1d4726952818c288388d20da1b0e998c
Thierry Martinez <thierry.martinez@inria.fr>
2025-06-17 00:33:45 +0200

    Use `from __future__ import annotations` for Python <3.10

--- rustworkx/__init__.py
+++ rustworkx/__init__.py
@@ -6,6 +6,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+from __future__ import annotations
 
 import importlib
 import sys
