514d38bca84a776a0620ef11231562302f1aec60
Ivan Carvalho <ivancarvalho@gatech.edu>
2025-07-06 18:55:57 -0400

    Add Python docstring

--- rustworkx/__init__.py
+++ rustworkx/__init__.py
@@ -2283,5 +2283,31 @@ def single_source_all_shortest_paths(
 
 @_rustworkx_dispatch
 def write_graphml(graph, path, /, keys=None, compression=None):
-    """ """
+    """Write a graph to a file in GraphML format.
+
+    GraphML is a comprehensive and easy-to-use file format for graphs. It consists
+    of a language core to describe the structural properties of a graph and a flexible
+    extension mechanism to add application-specific data.
+
+    For more information see:
+    http://graphml.graphdrawing.org/
+
+    .. note::
+
+        This implementation does not support mixed graphs (directed and undirected edges together),
+        hyperedges, nested graphs, or ports.
+
+    .. note::
+
+        GraphML attributes with `graph` domain are written from the graph's attrs field.
+
+    :param graph: The graph to write to the file. This can be a
+        :class:`~rustworkx.PyGraph` or :class:`~rustworkx.PyDiGraph`.
+    :param path: The path of the output file to write.
+    :param keys: Optional list of key definitions for GraphML attributes.
+        If not specified, keys will be inferred from the graph data.
+    :param compression: Optional compression format for the output file.
+        If not specified, no compression is applied.
+    :raises RuntimeError: when an error is encountered while writing the GraphML file.
+    """
     raise TypeError(f"Invalid Input Type {type(graph)} for graph")
