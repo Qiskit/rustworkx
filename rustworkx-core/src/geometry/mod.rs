8d2742d6b1a6212b43502434afc25db6e188ec48
Simon Lizotte <50745428+SILIZ4@users.noreply.github.com>
2025-09-28 21:35:05 -0400

    New function greedy routing (#1478)
    * add greedy routing and various distances to rustworkx-core; add hyperbolic greedy routing to rustworkx
    
    * add missing type specification
    
    * fix formatting from previous commit
    
    * fix doc indentation, fix stub naming incoherence
    
    ---------
    
    Co-authored-by: Ivan Carvalho <ivancarvalho@gatech.edu>

---
+++ rustworkx-core/src/geometry/mod.rs
@@ -0,0 +1,22 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+//! Module for graphs with vertices embedded in a metric space.
+
+mod distances;
+mod greedy_routing;
+
+pub use distances::{
+    angular_distance, euclidean_distance, hyperboloid_hyperbolic_distance, lp_distance,
+    maximum_distance, polar_hyperbolic_distance, IncompatiblePointsError,
+};
+pub use greedy_routing::{greedy_routing, greedy_routing_success_rate, NodeNotReachedError};
