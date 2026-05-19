cafad4fae17cba7f3640085b2f740c7df037c481
Cam Parry <cam.parry@kapiche.com>
2026-05-19 10:34:20 +1000

    Remove dead code from kamada_kawai_layout dispatcher
    The dispatcher fallthrough should only raise TypeError - the leftover
    networkx_converter body was unreachable after the raise and duplicated
    the real networkx_converter at line 948.

--- rustworkx/__init__.py
+++ rustworkx/__init__.py
@@ -1053,45 +1053,6 @@ def kamada_kawai_layout(
     :rtype: dict
     """
     raise TypeError(f"Invalid Input Type {type(graph)} for graph")
-    """Convert a networkx graph object into a rustworkx graph object.
-
-    .. note::
-
-        networkx is **not** a dependency of rustworkx and this function
-        is provided as a convenience method for users of both networkx and
-        rustworkx. This function will not work unless you install networkx
-        independently.
-
-    :param networkx.Graph graph: The networkx graph to convert.
-    :param bool keep_attributes: If ``True``, add networkx node attributes to
-        the data payload in the nodes of the output rustworkx graph. When set to
-        ``True``, the node data payloads in the output rustworkx graph object
-        will be dictionaries with the node attributes from the input networkx
-        graph where the ``"__networkx_node__"`` key contains the node from the
-        input networkx graph.
-
-    :returns: A rustworkx graph, either a :class:`~rustworkx.PyDiGraph` or a
-        :class:`~rustworkx.PyGraph` based on whether the input graph is directed
-        or not.
-    :rtype: :class:`~rustworkx.PyDiGraph` or :class:`~rustworkx.PyGraph`
-    """
-    if graph.is_directed():
-        new_graph = PyDiGraph(multigraph=graph.is_multigraph())
-    else:
-        new_graph = PyGraph(multigraph=graph.is_multigraph())
-    nodes = list(graph.nodes)
-    node_indices = dict(zip(nodes, new_graph.add_nodes_from(nodes)))
-    new_graph.add_edges_from(
-        [(node_indices[x[0]], node_indices[x[1]], x[2]) for x in graph.edges(data=True)]
-    )
-
-    if keep_attributes:
-        for node, node_index in node_indices.items():
-            attributes = graph.nodes[node]
-            attributes["__networkx_node__"] = node
-            new_graph[node_index] = attributes
-
-    return new_graph
 
 
 @_rustworkx_dispatch
